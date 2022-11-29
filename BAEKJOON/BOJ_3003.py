666a53d9287328a3b9863dcb51f5e3cb2f9ce4d9
ChanYoungHong <cksdud9883@naver.com>
2022-11-29 21:17:48 +0900

    킹 퀸 룩 비숍 나이트 폰 -> 기본 리스트 연습 함

--- BAEKJOON/BOJ_3003.py
+++ BAEKJOON/BOJ_3003.py
@@ -71,10 +71,25 @@
 # result = [i for i in a if i not in remove_set]
 # print(result)
 
+check = [1,1,2,2,2,8]
+a = [0,1,2,2,2,7]
+b = [2,1,2,1,2,1]
 
-print("         ,r'\"7")
-print("r`-_   ,'  ,/")
-print(" \\. \". L_r'")
-print("   `~\\/")
-print("      |")
-print("      |")
+answer = [i for i in range(6)]
+
+# 처음 머리 속에서 나오는 코드 그런데 배열? 리스트를 어떻게 출력해야하는지 몰라서 답변 참고
+# for i in range(6):
+#     if check[i] == b[i]:
+#         answer[i] = 0
+#     else:
+#         answer[i] = check[i] - b[i]
+#
+# print(answer)
+
+
+# 답변을 보고 list를 자바의 scanner처럼 어떻게 받는지 참고 함
+# list를 답변으로 어떻게 넣는 지 몰라서 못 품
+b = list(map(int, input().split()))
+
+for i in range(6):
+    print(check[i] - b[i], end=' ')
