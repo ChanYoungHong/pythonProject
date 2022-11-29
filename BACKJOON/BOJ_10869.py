605146a8c93f6767451be70ff35d87a8ff823893
ChanYoungHong <cksdud9883@naver.com>
2022-11-29 19:10:12 +0900

    파이썬 스터디 사칙연산 문제 풀이

---
+++ BACKJOON/BOJ_10869.py
@@ -0,0 +1,26 @@
+
+n, k = map(int, input().split())
+
+# 결과 값들을 나눠서 받기 위해서 분리 함
+result = 0
+result2 = 0
+result3 = 0
+result4 = 0
+result5 = 0
+
+result = n + k
+print(result)
+
+result2 = n - k
+print(result2)
+
+result3 = n*k
+print(result3)
+
+result4 = n//k
+print(result4)
+
+result5 = n%k
+print(result5)
+
+
