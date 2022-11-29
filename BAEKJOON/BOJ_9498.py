0dc6f5de2832bf9307a49f5f5d71be2ed3981f7c
ChanYoungHong <cksdud9883@naver.com>
2022-11-29 21:39:13 +0900

    시험 성적 문제풀이

---
+++ BAEKJOON/BOJ_9498.py
@@ -0,0 +1,15 @@
+
+# int 형으로 값을 어떻게 받는지 몰라서 검색 함!!
+score = int(input())
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score <= 89:
+    print("B")
+elif 70 <= score <= 79:
+    print("C")
+elif 60 <= score <= 69:
+    print("D")
+else:
+    print("F")
+
