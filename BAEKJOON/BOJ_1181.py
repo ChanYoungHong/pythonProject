bf78c2c272f1cc7961e7f2a1f88902cc36b373cc
ChanYoungHong <cksdud9883@naver.com>
2022-12-03 19:20:04 +0900

    다시 복습한 문제 12/3

--- BAEKJOON/BOJ_1181.py
+++ BAEKJOON/BOJ_1181.py
@@ -1,30 +1,71 @@
+#
+# N = int(input())
+# wordList = []
+#
+# for i in range(N):
+#     word = input()
+#     wordList.append(word)
+#
+# # set으로 중복을 제거 해야하는 것은 이해완료
+# set_word = list(set(wordList))
+#
+# # 여기서부터 어떻게 해야 할 지 모르겠음
+# sort_word = []
+# for i in set_word:
+#     sort_word.append((len(i), i))
+#
+# result = sorted(sort_word)
+#
+# for len_word, word in result:
+#     print(word)
+#
+#     # wordList.sort(reverse=True)
+#     # wordList.sort()
+#     # if len(wordList[i]) == len(wordList[i+1]):
+#     #     wordList.sort()
+#     # elif len(wordList[i]) > len(wordList[i+1]):
+#     #     wordList.sort()
+#     # else:
+#     #     continue
+#     # print(set_word[i])
+#
+#
 
+# 복습 완료
 N = int(input())
-wordList = []
+array = []
 
-for i in range(N):
+for i in range(N): # 0 ~ 13까지 0 1 2 3 4 5 ....
     word = input()
-    wordList.append(word)
+    array.append(word)
 
-# set으로 중복을 제거 해야하는 것은 이해완료
-set_word = list(set(wordList))
+set_array = list(set(array))
 
-# 여기서부터 어떻게 해야 할 지 모르겠음
-sort_word = []
-for i in set_word:
-    sort_word.append((len(i), i))
+sort_array = []
+for i in set_array:
+    sort_array.append((len(i), i))
 
-result = sorted(sort_word)
+result = sorted(sort_array)
 
 for len_word, word in result:
     print(word)
 
-    # wordList.sort(reverse=True)
-    # wordList.sort()
-    # if len(wordList[i]) == len(wordList[i+1]):
-    #     wordList.sort()
-    # elif len(wordList[i]) > len(wordList[i+1]):
-    #     wordList.sort()
-    # else:
-    #     continue
-    # print(set_word[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
