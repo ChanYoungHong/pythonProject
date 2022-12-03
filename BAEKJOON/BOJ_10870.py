200de132ae6241fd274f983002240b379d656f27
ChanYoungHong <cksdud9883@naver.com>
2022-12-03 20:18:43 +0900

    다시 복습한 문제 12/3 - 다시 풀기

--- BAEKJOON/BOJ_10870.py
+++ BAEKJOON/BOJ_10870.py
@@ -1,13 +1,11 @@
-품
-# 재귀함수 헷갈림
-# 시간 - 20분
 
-def ffibonnaccchi(N):
+
+
+def fibonnaccchi(N):
     if N <= 1:
         return N
-    return ffibonnaccchi(N-1) + ffibonnaccchi(N-2)
+    return fibonnaccchi(N-1) + fibonnaccchi(N-2)
+
 
 N = int(input())
-print(ffibonnaccchi(N))
-
-
+print(fibonnaccchi(10))
