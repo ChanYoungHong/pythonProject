979d3ba871b37fddde44ddf76f31db244840a6a3
ChanYoungHong <cksdud9883@naver.com>
2022-12-07 19:22:29 +0900

    블로그 문제 50개 - 백트래킹

--- BAEKJOON/BOJ_2675.py
+++ BAEKJOON/BOJ_2675.py
@@ -1,4 +1,4 @@
-
+#
 # 수도코드
 #
 # 1. N에 테스트케이스 개수를 입력을 받는다
@@ -19,13 +19,13 @@
 
 
 # 블로그 답안
-t = int(input())
-text = ''
-for i in range(t):
-    num, s = input().split()
-    for i in s:
-        text += int(num) * i # 처음에 i가 s의 길이만큼 올라가는데 어떻게 같은 문자가 반복이 가능한가 ? 라고 생각 함
-    print(text)
+# t = int(input())
+# text = ''
+# for i in range(t):
+#     num, s = input().split()
+#     for i in s:
+#         text += int(num) * i # 처음에 i가 s의 길이만큼 올라가는데 어떻게 같은 문자가 반복이 가능한가 ? 라고 생각 함
+#     print(text)
 
 # a = "charlie"
 # result = ''
@@ -33,6 +33,36 @@ for i in range(t):
 # for k in a: # 문자열을 넘으면 k 자리에 그 해당하는 문자열 index 0 1 2 3 4 차례대로 들어 감
 #     result += int(2) * k # 2 * k
 #     print(result)
+
+
+
+
+
+n = int(input())
+answer = ''
+
+for i in range(n):
+    num, word = input().split()
+    for j in word:
+        answer += int(num) * j
+    print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
