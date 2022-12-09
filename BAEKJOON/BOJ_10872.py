1364b0075083692c1378c8b448e71014e237c71b
ChanYoungHong <cksdud9883@naver.com>
2022-12-09 16:12:01 +0900

    12월 9일 - 2번째 복습 완료

--- BAEKJOON/BOJ_10872.py
+++ BAEKJOON/BOJ_10872.py
@@ -1,44 +1,29 @@
-# # N = int(input())
-# #
-# # # 내 답안
-# # for i in range(1, 10):
-# #     def add(N):
-# #         return N * (N - i)
-# #         sum += add(N)
-# #
-# # print(sum)
-#
-# # 블로그 답안
-# def fac(n):
-#     result = 1
-#     if n > 0:
-#         result = n * fac(n-1)
-#     return result
-#
-# n = int(input())
-# print(fac(n))
-#
-# # 15분 정도 걸림, 답 보고 품
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
 # 12월 3일 복습
-N = int(input())
+# N = int(input())
 
-def fac(N):
+# def fac(N):
+#
+#     if N == 0:
+#         return 1
+#
+#     return N * fac(N-1)
+#
+# print(fac(N))
 
-    if N == 0:
+def solution(n):
+    # 1.
+    if n > 0:
+        return n
+    else:
         return 1
 
-    return N * fac(N-1)
+    # 2.
+    if n == 0:
+        return 1
 
-print(fac(N))
+    return n * solution(n-1)
+
+
+print(solution(10))
+print(solution(0))
+print(solution(1))
