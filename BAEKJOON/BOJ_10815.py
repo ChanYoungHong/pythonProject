5058f5b292dbd838c635f2d3a2fac421512e6a9a
ChanYoungHong <cksdud9883@naver.com>
2022-12-10 14:41:33 +0900

    12월 10일 2차 복습 완료

--- BAEKJOON/BOJ_10815.py
+++ BAEKJOON/BOJ_10815.py
@@ -1,24 +1,27 @@
-myNum = int(input())
-myCard = list(map(int, input().split()))
+n1 = int(input())
+card = list(map(int, input().split()))
 
-yourNum = int(input())
-yourDeck = list(map(int, input().split()))
+n2 = int(input())
+check = list(map(int, input().split()))
 
-myCard.sort() # -10 2 3 6 10
+card.sort()
 
-def binary_search(array, target,start, end):
+def binary_search(card, target, start, end):
+
     while start <= end:
+
         mid = (start + end) // 2
-        if array[mid] == target:
+
+        if card[mid] == target:
             return mid
-        elif array[mid] < target:
-            return mid + 1
+        elif card[mid] < target:
+            start = mid + 1
         else:
-            return mid - 1
+            end = mid -1
     return None
 
-
-# binary_search(myNum, yourDeck[i],0, myNum-1)
-#
-# for i in range(n):
-
+for i in range(n2):
+    if binary_search(card, check[i], 0, n1 - 1) is not None:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
