6093eec0d60f1977c6dca755901528eadd8c2a48
ChanYoungHong <cksdud9883@naver.com>
2022-12-11 01:31:04 +0900

    12월 11일 3차 풀이완료 - 집중

--- BAEKJOON/BOJ_2675.py
+++ BAEKJOON/BOJ_2675.py
@@ -6,16 +6,16 @@
 # 3. num 횟수만큼 ABC 순서대로 넣고 반복하여 준다.
 # 4. 각각의 index의 자리를 3번씩 반복 해줘야 한다.
 # 5.
-
-t = int(input())
-
-result = "";
-for i in range(t):
-    num, s = input().split()
-
-    for j in num:       #              #          0 1 2
-        result += s[j]                  #         A B C
-    print(result)
+#
+# t = int(input())
+#
+# result = "";
+# for i in range(t):
+#     num, s = input().split()
+#
+#     for j in num:       #              #          0 1 2
+#         result += s[j]                  #         A B C
+#     print(result)
 
 
 # 블로그 답안
@@ -26,26 +26,27 @@ for i in range(t):
 #     for i in s:
 #         text += int(num) * i # 처음에 i가 s의 길이만큼 올라가는데 어떻게 같은 문자가 반복이 가능한가 ? 라고 생각 함
 #     print(text)
-#
+
 # a = "charlie"
 # result = ''
 #
 # for k in a: # 문자열을 넘으면 k 자리에 그 해당하는 문자열 index 0 1 2 3 4 차례대로 들어 감
 #     result += int(2) * k # 2 * k
 #     print(result)
+import sys
 
-
-
+input = sys.stdin.readline
 
 
 n = int(input())
-answer = ''
 
+# text = "" # 여기 text 위치가 있으면 출력 초과가 뜸, print가 3번이 됨
 for i in range(n):
-    num, word = input().split()
-    for j in word:
-        answer += int(num) * j
-    print(answer)
+    a, b = input().split()
+    text = ""
+    for i in b:
+        text += i * int(a)
+    print(text)
 
 
 
