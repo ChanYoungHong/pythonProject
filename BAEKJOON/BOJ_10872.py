9ff848ce793f0d3fec375d9e9f4e4ee53f678ce5
ChanYoungHong <cksdud9883@naver.com>
2022-12-14 12:03:36 +0900

    12월 14일 수 - 복습 완료

--- BAEKJOON/BOJ_10872.py
+++ BAEKJOON/BOJ_10872.py
@@ -1,29 +1,10 @@
-# 12월 3일 복습
-# N = int(input())
 
-# def fac(N):
-#
-#     if N == 0:
-#         return 1
-#
-#     return N * fac(N-1)
-#
-# print(fac(N))
+n = int(input())
+def fibbonachi(n):
 
-def solution(n):
-    # 1.
-    if n > 0:
-        return n
-    else:
-        return 1
-
-    # 2.
     if n == 0:
         return 1
+    return n * fibbonachi(n-1)
 
-    return n * solution(n-1)
 
-
-print(solution(10))
-print(solution(0))
-print(solution(1))
+print(fibbonachi(n))
