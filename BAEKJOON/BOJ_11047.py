627476ea937efbf3568ec11a6b553b6eeab61d82
ChanYoungHong <cksdud9883@naver.com>
2022-12-16 19:46:12 +0900

    12/16 금 - 구현 문제, 복습 풀이 좀 참고 함

--- BAEKJOON/BOJ_11047.py
+++ BAEKJOON/BOJ_11047.py
@@ -1,23 +1,18 @@
-
-# 10, 4200
-n, m = list(map(int, input().split()))
+n, m = map(int, input().split())
 array = []
-answer = 0
+cnt = 0
+result = 0
 
 for i in range(n):
-    nums = int(input())
-    array.append(nums)
-    array.sort(reverse=True)
+    array.append(int(input()))
 
-    # if m >= array[i]:                # for문 하나로 돌리게 되면 배열에 하나 밖에 담겨 있어서 출력이 안 됨, 그래서 for문 2개를 사용해야 함
-    #     answer += m // array[i]
-    #     m %= array[i]
-    #     print(array[i])
+array.sort(reverse=True)
 
-for j in range(n):
-    if m >= array[j]:  # for문 하나로 돌리게 되면 배열에 하나 밖에 담겨 있어서 출력이 안 됨, 그래서 for문 2개를 사용해야 함
-        answer += m // array[j]
-        m %= array[j]
+for i in range(n):
+    if m >= array[i]:
+        cnt += (m // array[i])
+        m %= array[i]
 
-print(answer)
-print(array)
+
+print(cnt)
+print(result)
