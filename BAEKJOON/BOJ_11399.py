5582d8e2ecd15ab42a2ac976a38dce17864671bd
ChanYoungHong <cksdud9883@naver.com>
2022-12-16 18:59:27 +0900

    12/16 금 - 그리디 문제 복습

--- BAEKJOON/BOJ_11399.py
+++ BAEKJOON/BOJ_11399.py
@@ -1,11 +1,22 @@
 
+# 2 5 1 4 3이 되어야 함
+
+#사람 1 2 3 4 5
+#시간 3 1 4 3 2
+
 n = int(input())
-array = list(map(int, input().split()))
+a = list(map(int, input().split()))
+
+a.sort()
 result = 0
 
-array.sort()
-for i in range(n):
+for i in range(n): # 0
     for j in range(i+1):
-        result += array[j]
+        result += a[j]
 
 print(result)
+
+
+
+
+
