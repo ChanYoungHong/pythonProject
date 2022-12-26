5c0e6506b02a90a3fb30294ee6966f8795099a67
ChanYoungHong <cksdud9883@naver.com>
2022-12-26 16:21:40 +0900

    12/26 월 - 백트래킹 문제 품

---
+++ BAEKJOON/백트래킹/BOJ_6603.py
@@ -0,0 +1,23 @@
+
+
+def dfs(dep, idx):
+
+    if dep == 6:
+        print(*result)
+        return
+
+    for i in range(idx, k):
+        result.append(s[i])
+        dfs(dep + 1, i + 1)
+        result.pop()
+
+
+while True:
+    array = list(map(int, input().split()))
+    k = array[0]
+    s = array[1:]
+    result = []
+    dfs(0,0)
+    if k == 0:
+        exit()
+    print()
