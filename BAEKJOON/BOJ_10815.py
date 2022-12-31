0e7ab0a3d897e2f54465566c34725edb355f0eb5
ChanYoungHong <cksdud9883@naver.com>
2022-12-31 17:58:27 +0900

    12/31 토 -> 숫자 카드 / 실버 5

--- BAEKJOON/BOJ_10815.py
+++ BAEKJOON/BOJ_10815.py
@@ -1,27 +1,28 @@
-n1 = int(input())
-card = list(map(int, input().split()))
+n = int(input())  # 5개 상근이 가진 카드
+array1 = list(map(int, input().split()))
 
-n2 = int(input())
-check = list(map(int, input().split()))
+m = int(input())  # 8개 상근이 가졌는지 확인해야 할 카드
+array2 = list(map(int, input().split()))
 
-card.sort()
+array1.sort()
 
-def binary_search(card, target, start, end):
+
+def binary_search(array1, target, start, end):
 
     while start <= end:
-
         mid = (start + end) // 2
 
-        if card[mid] == target:
+        if array1[mid] == target:
             return mid
-        elif card[mid] < target:
-            start = mid + 1
+        elif array1[mid] > target:
+            end = mid - 1
         else:
-            end = mid -1
+            start = start + 1
+
     return None
 
-for i in range(n2):
-    if binary_search(card, check[i], 0, n1 - 1) is not None:
+for i in range(m):
+    if binary_search(array1,array2[i],0,n-1) is not None:
         print(1, end=' ')
     else:
         print(0, end=' ')
