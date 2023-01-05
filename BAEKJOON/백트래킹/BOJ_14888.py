5db042efe237d0ff6ebfccb08be3434446951e70
ChanYoungHong <cksdud9883@naver.com>
2023-01-05 20:01:15 +0900

    1/5 -> dfs, 코딩 연습 !!!

--- BAEKJOON/백트래킹/BOJ_14888.py
+++ BAEKJOON/백트래킹/BOJ_14888.py
@@ -1,11 +1,11 @@
-n = int(input())
+import sys
 
+n = int(input())
 nums = list(map(int, input().split()))
 oper = list(map(int, input().split()))
 
 maxx = -1e9
 minn = 1e9
-
 
 def dfs(dep, total, add, minus, multi, divide):
     global maxx, minn
@@ -17,10 +17,13 @@ def dfs(dep, total, add, minus, multi, divide):
 
     if add:
         dfs(dep + 1, total + nums[dep], add - 1, minus, multi, divide)
+
     if minus:
         dfs(dep + 1, total - nums[dep], add, minus - 1, multi, divide)
+
     if multi:
         dfs(dep + 1, total * nums[dep], add, minus, multi - 1, divide)
+
     if divide:
         dfs(dep + 1, int(total / nums[dep]), add, minus, multi, divide - 1)
 
