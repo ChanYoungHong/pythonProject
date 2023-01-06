0fd969a5ed44d499df4f4cedb4a3a68adaefbbf0
ChanYoungHong <cksdud9883@naver.com>
2023-01-06 10:54:49 +0900

    1/6 -> dfs, 응용 복습 완료

---
+++ BAEKJOON/DFS/BOJ_1987_알파벳.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [list(input()) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+array = set()
+answer = 0
+
+
+def dfs(x, y, cnt):
+
+    global answer
+    answer = max(answer, cnt)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m and not graph[nx][ny] in array:
+            array.add(graph[nx][ny])
+            dfs(nx,ny, cnt + 1)
+            array.remove(graph[nx][ny])
+
+
+
+array.add(graph[0][0])
+dfs(0, 0, 1)
+print(answer)
