aaf99aaffa9dda2d4754ec1d5e99ee659db18566
ChanYoungHong <cksdud9883@naver.com>
2023-01-06 20:57:17 +0900

    1/6 -> 구간합 구하기 복습 완료

--- BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
+++ BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
@@ -3,17 +3,16 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = list(map(int, input().split())) # 5 4 3 2 1
+
+nums = list(map(int, input().split()))
 
 sum = 0
-ch_nums = [0]
+result = [0]
+for i in nums:
+    sum += i
+    result.append(sum)
+    print(sum)
 
-for i in range(n):
-  sum += nums[i]
-  ch_nums.append(sum)
-
-result = 0
-for i in range(m):
-  a, b = map(int, input().split())
-  result = ch_nums[b] - ch_nums[a-1]
-  print(result)
+for _ in range(m):
+    a, b = map(int, input().split())
+    print(result[b] - result[a-1])
---
+++ BAEKJOON/누적합 구하기/BOJ_11660_구간합구하기5.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+
+dp = [[0]*(n+1) for i in range(n+1)]
+
+
+for i in range(1, n+1): # 1 2 3 4
+    for j in range(1, n+1): # 1 2 3 4
+        dp[i][j] = dp[i][j - 1] + dp[i-1][j] - dp[i-1][j-1] + graph[i-1][j-1]
+
+
+for _ in range(m):
+    x1,y1,x2,y2 = map(int, input().split())
+
+    result = dp[x2][y2] - dp[x2][y1 -1] - dp[x1 -1 ][y2] + dp[x1-1][y1-1]
+    print(result)
+
+
+# sum = 0
+# for i in range(a-1, c-1): # 2 3
+#     for j in range(b-1, d-1): #
+#        sum += graph[i][j]
+#
+# print(sum)
+
