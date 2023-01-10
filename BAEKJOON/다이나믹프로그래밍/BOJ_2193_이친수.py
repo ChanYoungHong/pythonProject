bfc3c33e5c70bf3fe143f3e444d2d729017cb13c
ChanYoungHong <cksdud9883@naver.com>
2023-01-10 11:55:56 +0900

    1/10 화 -> dp 이친수 복습 완료 + 원리 파악하기

--- BAEKJOON/다이나믹프로그래밍/BOJ_2193_이친수.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_2193_이친수.py
@@ -1,32 +1,12 @@
-'''
-
-1. 0으로 시작하면 안 됨
-2. 1이 연속으로 이어지면 안 됨
-
-'''
-
 import sys
 
-input = sys.stdin.readline
-#
-# n = int(input())
-#
-# cntArray = []
-# result = []
-# for i in range(n):
-#
-#     if i == 0:
-#         result += '1'
-#     else:
-#         result += '0'
-# cntArray.append(result)
-#
-# print(result)
-# print(len(cntArray))
-# print(cntArray)
-
-s = [0, 1, 1]
-for i in range(3, 91):
-  s.append(s[i - 2] + s[i - 1])
 n = int(input())
-print(s[n])
+
+dp = [0] * (n+1)
+dp[1] = 1
+
+for i in range(2, n+1):
+  dp[i] = dp[i-2] + dp[i-1]
+
+print(dp[n])
+print(dp)
