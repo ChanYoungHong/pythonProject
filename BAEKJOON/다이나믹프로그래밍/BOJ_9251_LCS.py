d6cea27ef50278d90e25d8b0361c264ffb325a91
ChanYoungHong <cksdud9883@naver.com>
2023-01-11 16:14:49 +0900

    1/11 수 -> dp, LCS 복습 완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_9251_LCS.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_9251_LCS.py
@@ -1,18 +1,17 @@
 import sys
 
-input = sys.stdin.readline
-
-word1 = input().strip()
-word2 = input().strip()
+word1 = input()
+word2 = input()
 
 a, b = len(word1), len(word2)
-dp = [[0] * (b + 1) for _ in range(a + 1)]
 
-for i in range(1, a + 1):
-    for j in range(1, b + 1):
-        if word1[i - 1] == word2[j - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
+array = [[0] * (b+1) for _ in range(a+1)]
+
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        if word1[i-1] == word2[j-1]:
+            array[i][j] = array[i-1][j-1] + 1
         else:
-            dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+            array[i][j] = max(array[i][j-1], array[i-1][j])
 
-print(dp[-1][-1])
+print(array[-1][-1])
