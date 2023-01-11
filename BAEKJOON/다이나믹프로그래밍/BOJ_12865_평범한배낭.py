fb6bed500ff14d452211c0a255ca27ffb392782e
ChanYoungHong <cksdud9883@naver.com>
2023-01-11 20:40:59 +0900

    1/11 수 -> dp, 배낭 문제 - 냅색 알고리즘 복습 완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_12865_평범한배낭.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_12865_평범한배낭.py
@@ -1,21 +1,25 @@
 import sys
-from collections import deque
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-nums = []
+stuff = [[0,0]]
 for _ in range(n):
-    w, v = map(int, input().split())
-    nums.append((w,v))
-
-dp = [[0]*(len(nums[0])) for _ in range(n)]
-
-for i in range(n):
-    for j in range(len(nums[0])):
-        dp[m] = dp[i-2][0] + dp[i-1][0]
-
-print(nums)
-# print(dp[m])
+    stuff.append(list(map(int, input().split())))
 
 
+knapsack = [[0] * (m+1) for _ in range(n+1)]
 
+for i in range(1, n+1):
+    for j in range(1, m+1):
+
+        w = stuff[i][0]
+        v = stuff[i][1]
+
+        if j < w:
+            knapsack[i][j] = knapsack[i-1][j]
+        else:
+            knapsack[i][j] = max(v + knapsack[i-1][j-w], knapsack[i-1][j])
+
+print(knapsack[n][m])
