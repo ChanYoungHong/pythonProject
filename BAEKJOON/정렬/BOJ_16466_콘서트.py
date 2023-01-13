89bcbb063fac3cbe6994bb6e6e1ff8e834fda33c
ChanYoungHong <cksdud9883@naver.com>
2023-01-13 10:49:43 +0900

    1/13 금 -> 정렬-스터디문제, 복습

--- BAEKJOON/정렬/BOJ_16466_콘서트.py
+++ BAEKJOON/정렬/BOJ_16466_콘서트.py
@@ -5,35 +5,20 @@
 
 '''
 
-#
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
 
-soldTicket = list(map(int, input().split()))
-totalTicket = []
+nums = sorted(list(map(int, input().split())))
 
-soldTicket.sort()
-
-for i in range(1, max(soldTicket) + 1):
-    totalTicket.append(i)
-
-for j in range(len(soldTicket) + 1):
-    if totalTicket[j] != soldTicket[j]:
-        print(totalTicket[j])
+is_full = 1
+for i in range(len(nums)):
+    if (i+1) != nums[i]:
+        print(i+1)
+        is_full = 0
         break
 
 
-# import sys
-# n=int(sys.stdin.readline())
-# nums=sorted(list(map(int, sys.stdin.readline().split())))
-# is_full=1
-# for i in range(len(nums)):
-#     if (i+1)!=nums[i]:
-#         print(i+1)
-#         is_full=0
-#         break
-# if is_full == 1:
-#     print(n+1)
+if is_full == 1:
+    print(n+1)
