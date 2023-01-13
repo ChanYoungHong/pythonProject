45165a2da8e73e85b9b9f5ebd04afb756ee75aef
ChanYoungHong <cksdud9883@naver.com>
2023-01-13 10:13:33 +0900

    1/13 금 -> 정렬-스터디문제, 복습

--- BAEKJOON/정렬/BOJ_6996_애너그램.py
+++ BAEKJOON/정렬/BOJ_6996_애너그램.py
@@ -1,7 +1,7 @@
 '''
 
 1. 반복문을 두 개 돌려서 하나씩 비교하는 방법으로 생각 함
-2. 오른쪽 값을 기준으로 왼쪽 값을 넣었을 때 그 길이가 오른쪽 값의 길이와 같으면 애니그램으로 출
+2. 오른쪽 값을 기준으로 왼쪽 값을 넣었을 때 그 길이가 오른쪽 값의 길이와 같으면 애니그램으로 출력
 
 '''
 
@@ -9,41 +9,21 @@ import sys
 
 input = sys.stdin.readline
 
+n = int(input().rstrip())
 
-n = int(input())
+array1 = []
+array2 = []
 
-for i in range(n):
-    t, h = map(str, input().split())
+for _ in range(n):
+    a, b = map(str, input().split())
 
-    x = sorted(list(t))
-    y = sorted(list(h))
+    x = list(a)
+    y = list(b)
+
+    x.sort()
+    y.sort()
 
     if x == y:
-        print("%s & %s are anagrams." % (t, h))
+        print("%s & %s are anagrams." % (a, b))
     else:
-        print("%s & %s are NOT anagrams." % (t, h))
-
-# n = int(input())
-#
-# array = []
-# result = []
-# for _ in range(n):
-#     array.append(list(map(str, input().split())))
-#
-#
-# for i in array[0][1]:
-#     for j in array[0][0]:
-#         if i == j:
-#             result.append(j)
-#
-# if len(result) == len(array[0][1]):
-#     print('blather & reblath are anagrams.')
-# else:
-#     print('')
-#
-# print(result)
-#
-# for i in range(2):
-#     for j in range()
-#
-
+        print("%s & %s are NOT anagrams." % (a, b))
