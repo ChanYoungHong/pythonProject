e1b073309466dbe782440f8c38df454553aa9a5e
ChanYoungHong <cksdud9883@naver.com>
2023-01-16 18:50:06 +0900

    1/16 월 -> 백트래킹, n과 m 1문제 복습 완료

--- BAEKJOON/백트래킹/BOJ_15649.py
+++ BAEKJOON/백트래킹/BOJ_15649.py
@@ -1,22 +1,23 @@
+
 import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split())
-visited = [False] * (n + 1)
+n, m = map(int, input().split())
+visited = [False] * (n+1)
 result = []
 
-
 def dfs(dep):
+
     if dep == m:
         print(' '.join(map(str, result)))
         return
 
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         if visited[i] == False:
             visited[i] = True
             result.append(i)
-            dfs(dep+1)
+            dfs(dep + 1)
             visited[i] = False
             result.pop()
 
