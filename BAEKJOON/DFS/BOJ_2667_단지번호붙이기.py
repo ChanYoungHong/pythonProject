b26d09eefe48a3f0362dd81a74ed52c60f84ef08
ChanYoungHong <cksdud9883@naver.com>
2023-01-16 20:40:21 +0900

    1/16 월 -> bfs, 단지 번호 붙이기 복습

---
+++ BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
@@ -0,0 +1,52 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+
+# for _ in range(n):
+#     graph.append(list(map(int, input().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+visited = [[False] * n for _ in range(n)]
+result = []
+cnt2 = 0
+
+def bfs(x, y):
+    cnt = 1
+    q = deque()
+    q.append((x, y))
+    graph[x][y] = 0
+    while q:
+
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                q.append((nx, ny))
+                cnt += 1
+    result.append(cnt)
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            bfs(i, j)
+            cnt2 += 1
+
+result.sort()
+
+print(cnt2)
+for i in result:
+    print(i)
