21cd36a74105a32bbbb018dd369712f6752d95b0
ChanYoungHong <cksdud9883@naver.com>
2023-01-25 22:25:24 +0900

    1/25 수 -> 코드 뜯고 분석해보기

--- BAEKJOON/BFS/BOJ_16236_아기상어.py
+++ BAEKJOON/BFS/BOJ_16236_아기상어.py
@@ -1,76 +1,94 @@
-from collections import deque
+'''
+1. 시간복잡도 : bfs를 써서 아무렇게나 짜면 됨, 왜 bfs를 써야하나 ?
+2. 아이디어 : 조건 그대로 코드를 짜면 될 듯 ..
+더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청한다.
+먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
+먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
+거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
+거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
+'''
 
-INF = 1e9
+'''
+
+1 추가적인 디테일 -> 아기상어가 먹은 후에는 그 자리는 0이 되어야 함.
+2 일단 물고기가 있는지 없는지 확인을 해야 함
+
+'''
+from collections import deque
+import sys
+
+input = sys.stdin.readline
 
 n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+babyshark = 2
+dist = 0
+move_num = 0
+eat = 0
 
-array = []
-for i in range(n):
-    array.append(list(map(int, input().split())))
+INF = sys.maxsize
 
-now_size = 2
-now_x, now_y = 0,0
-
-for i in range(n):
-    for j in range(n):
-        if array[i][j] == 9:
-            now_x, now_y = i, j
-            array[now_x][now_y] = 0
-
+size = 0
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+# 추가
+sx, sy = 0,0
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 9:
+            graph[i][j] = 0
+            sx, sy = i, j
+            break
+
+        # if graph[i][j] == 0:
+        #     print(0)
+        #     # 종료 시키기
+        # elif graph[i][j] == 9:
+        #     q.append((i, j))
 def bfs():
-    dist = [[-1] * n for _ in range(n)]
-    q = deque([(now_x, now_y)])
-    dist[now_x][now_y] = 0
+
+    q = deque()
+    q.append((sx, sy, 0))
+    visited = [[False] * n for _ in range(n)]
+    flag = 1e9
+    fish = []
 
     while q:
-        x,y = q.popleft()
+        global babyshark, dist, move_num, move, eat
+
+        x, y, count = q.popleft()
+
+        if count > flag:
+            break
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx and nx < n and 0 <= ny and ny < n:
-                if dist[nx][ny] == -1 and array[nx][ny] <= now_size:
-                    dist[nx][ny] = dist[x][y] + 1
-                    q.append((nx, ny))
 
-    return dist
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+                continue
 
-def find(dist):
-    x,y = 0,0
-    min_dist = INF
-    for i in range(n):
-        for j in range(n):
-            if dist[i][j] != -1 and 1 <= array[i][j] and array[i][j] < now_size:
+            if graph[nx][ny] > size or visited[nx][ny]:
+                continue
 
-                if dist[i][j] < min_dist:
-                    x, y = i, j
-                    min_dist = dist[i][j]
+            ''' 아기상어 레벨이 2이라는 것을 표시를 해줘야 함.. 어떻게 함 ? '''
 
-    if min_dist == INF:
-        return None
-    else:
-        return x,y,min_dist
+            if graph[nx][ny] != 0 and graph[nx][ny] < size:
+                fish.append((nx, ny, count+1))
+                flag = count
+            visited[nx][ny] = True
+            q.append((nx,ny,count + 1))
 
+            # elif graph[nx][ny] == babyshark:
+            #     '''지나가는 것을 어떻게 표현 함 ?? '''
+            # elif graph[nx][ny] > babyshark:
+            #     continue
 
-result = 0
-ate = 0
-
-while True:
-
-    value = find(bfs())
-
-    if value == None:
-        print(result)
-        break
-    else:
-        now_x, now_y = value[0], value[1]
-        result += value[2]
-
-        array[now_x][now_y] = 0
-        ate += 1
-
-        if ate >= now_size:
-            now_size += 1
-            ate = 0
+    if len(fish) > 0:
+        fish.sort()
+        x, y, move = fish[0][[0], fish[0][1], fish[0][2]
+        move_num += move
+        eat += 1
+        graph[0][0] = 0
