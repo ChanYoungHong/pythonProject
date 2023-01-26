369ad6dbda564b2e816b0910b6edf938503d1c16
ChanYoungHong <cksdud9883@naver.com>
2023-01-26 15:21:57 +0900

    1/26 목 -> 다시 복습 완료

--- BAEKJOON/백트래킹/BOJ_6603_로또문제.py
+++ BAEKJOON/백트래킹/BOJ_6603_로또문제.py
@@ -1,26 +1,33 @@
+'''
+
+1. 시간복잡도 -
+2. 아이디어 - 백트래킹을 사용해야 함
+
+'''
+
 import sys
 
 input = sys.stdin.readline
 
-result = []
 
 def dfs(dep, idx):
     if dep == 6:
         print(*result)
-        return
 
     for i in range(idx, k):
-        result.append(s[i])
+        result.append(nums[i])
         dfs(dep + 1, i + 1)
         result.pop()
 
+
 while True:
+
     array = list(map(int, input().split()))
     k = array[0]
-    s = array[1:]
+    nums = array[1:]
+    result = []
     dfs(0, 0)
+
     if k == 0:
         exit()
     print()
-
-
