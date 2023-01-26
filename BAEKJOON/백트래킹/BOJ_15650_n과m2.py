f0ecc5b5233e65c3fc8e75949f492aa3f29e9d29
ChanYoungHong <cksdud9883@naver.com>
2023-01-26 21:56:43 +0900

    1/26 목 -> 백트래킹 5번째 복습

--- BAEKJOON/백트래킹/BOJ_15650_n과m2.py
+++ BAEKJOON/백트래킹/BOJ_15650_n과m2.py
@@ -1,25 +1,21 @@
-'''
-1. 중복을 제거해야 하는데 그걸 어떻게 하지?
-2.
-'''
-
 import sys
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 result = []
-def dfs(start):
+
+
+def dfs(dep):
 
     if len(result) == m:
-        print(' '.join(map(str, result)))
+        print(*result)
         return
 
-    for i in range(start, n+1):
+    for i in range(dep, n+1):
         if i not in result:
             result.append(i)
-            dfs(i+1)
+            dfs(i + 1)
             result.pop()
 
 dfs(1)
-
