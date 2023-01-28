b9f73455e99d34c9911cbe8130f90c376feb77eb
ChanYoungHong <cksdud9883@naver.com>
2023-01-28 13:39:20 +0900

    1/28 토 -> 구현 문제 복습완료

--- BAEKJOON/구현/BOJ_8979_올림픽.py
+++ BAEKJOON/구현/BOJ_8979_올림픽.py
@@ -1,41 +1,33 @@
-'''
-
-1. 시간복잡도 - n < 1000아라 2중 for문을 사용해도 됨
-2. 아이디어 - m으로 지정 된 나를 골라서 기준으로 만들어서 구분한다.
-
-'''
-
 import sys
 
 input = sys.stdin.readline
-n, m = map(int,input().split())
 
+n, m = map(int, input().split())
+
+cnt = 0
 result = []
-
 for i in range(n):
     ranking = list(map(int, input().split()))
+    country = ranking[0]
+    rank = ranking[1:]
 
-    if ranking[0] != m:
+    if country != m:
         result.append(ranking)
     else:
         standard = ranking
 
-'''
-등수는 자신보다 더 잘한 나라 수 + 1
-cnt = 1이 되어야한다.
-'''
 
-cnt = 1 # 등수
+cnt = 1
 for i in range(len(result)):
     if result[i][1] > standard[1]:
         cnt += 1
-
     elif result[i][1] == standard[1]:
         if result[i][2] > standard[2]:
             cnt += 1
-
         elif result[i][2] == standard[2]:
             if result[i][3] > standard[3]:
                 cnt += 1
 
 print(cnt)
+
+
