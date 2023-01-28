a84fbe3e33d684b2c73fbbcf3ba6c175b4671b67
ChanYoungHong <cksdud9883@naver.com>
2023-01-28 11:53:57 +0900

    1/28 토 -> 백트래킹 복습 완료

--- BAEKJOON/백트래킹/BOJ_15649_n과m.py
+++ BAEKJOON/백트래킹/BOJ_15649_n과m.py
@@ -1,21 +1,22 @@
-
 import sys
 
 input = sys.stdin.readline
 
 n, m = map(int,input().split())
+
 result = []
-visited = [False] * (n)
+visited = [False] * (n+1)
 def dfs(dep):
 
     if dep == m:
         print(*result)
+        print(' '.join(map(str, result))) # 이 것을 사용하면 더 빠름
         return
 
-    for i in range(n):
+    for i in range(1, n+1):
         if visited[i] == False:
             visited[i] = True
-            result.append(i+1)
+            result.append(i)
             dfs(dep+1)
             visited[i] = False
             result.pop()
