1642de6606fce20a423ab6ad6c52ab96f80f1228
ChanYoungHong <cksdud9883@naver.com>
2023-01-30 17:44:18 +0900

    1/30 월 ->  순열파트 기본 문제 풀이 완료

---
+++ BAEKJOON/백트래킹/BOJ_10974_모든순열.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+result = []
+def dfs(dep):
+
+    if dep == n:
+        print(*result)
+        return
+
+    for i in range(1, n+1):
+        if i not in result:
+            result.append(i)
+            dfs(dep+1)
+            result.pop()
+
+dfs(0)
