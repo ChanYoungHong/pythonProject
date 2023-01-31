73f5fef025201bf1ea6706e91d33980ca2c99c29
ChanYoungHong <cksdud9883@naver.com>
2023-01-31 11:46:22 +0900

    1/30 월 -> 기본 구현문제

--- BAEKJOON/구현/BOJ_8979_올림픽.py
+++ BAEKJOON/구현/BOJ_8979_올림픽.py
@@ -3,24 +3,22 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-
-cnt = 0
+graph = []
 result = []
 for i in range(n):
-    ranking = list(map(int, input().split()))
-    country = ranking[0]
-    rank = ranking[1:]
 
-    if country != m:
-        result.append(ranking)
+    graph = list(map(int, input().split()))
+
+    if graph[0] != m:
+        result.append(graph)
     else:
-        standard = ranking
-
+        standard = graph
 
 cnt = 1
 for i in range(len(result)):
     if result[i][1] > standard[1]:
         cnt += 1
+
     elif result[i][1] == standard[1]:
         if result[i][2] > standard[2]:
             cnt += 1
@@ -28,6 +26,5 @@ for i in range(len(result)):
             if result[i][3] > standard[3]:
                 cnt += 1
 
+
 print(cnt)
-
-
