d7f9e20abc1c90e3a1760fd8ada5a19234a68352
ChanYoungHong <cksdud9883@naver.com>
2023-01-31 14:54:00 +0900

    1/31 화 -> 기본구현 문제

--- BAEKJOON/그리디/BOJ_14916_거스름돈.py
+++ BAEKJOON/그리디/BOJ_14916_거스름돈.py
@@ -4,34 +4,39 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [-1] * 100001
+cnt = 0
 
-dp[2] = dp[5] = 1
-dp[4] = 2
-dp[6] = 3
-dp[7] = 2
-dp[8] = 4
+while True:
 
-for i in range(9, n + 1):
-    dp[i] = min(dp[i - 2], dp[i - 5]) + 1
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+    elif n % 2 == 0:
+        n -= 2
+        cnt += 1
+    else:
+        n -= 2
+        cnt += 1
 
-print(dp[n])
+if n < 0:
+    print(-1)
+else:
+    print(cnt)
 
-# cnt = 0
+
+
+# dp = [-1] * 100001
+# dp[2] = 1
+# dp[5] = 1
 #
-# while True:
 #
-#     if n % 5 == 0:
-#         cnt += n//5
-#         break
-#     elif n % 2 == 0:
-#         n -= 2
-#         cnt += 1
-#     else:
-#         n -= 5
-#         cnt += 1
+# for i in range(6, n+1):
 #
-# if n < 0:
-#     print(-1)
-# else:
-#     print(cnt)
+#     if i % 5 == 0:
+#         dp[i] = dp[i-5] + 1
+#     elif i % 2 == 0:
+#         dp[i] = dp[i-2] + 1
+#     elif dp[i-2] > 0 and dp[i-5] > 0:
+#         dp[i] = min(dp[i-2], dp[i-5]) + 1
+#
+# print(dp[n])
