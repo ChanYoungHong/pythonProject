c663056798e24bb6793ff179a8531d7540bcfd3c
ChanYoungHong <cksdud9883@naver.com>
2023-02-01 22:43:24 +0900

    2/1 수 -> dp 가장 긴 증가하는 부분수열 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -1,14 +1,17 @@
+
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
-dp = [0] * n
+
+dp = [1] * (n+1)
 
 for i in range(n):
     for j in range(i):
-        if nums[i] > nums[j] and dp[i] < dp[j]:
-            dp[i] = dp[j]
-    dp[i] += 1
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
 print(max(dp))
+
