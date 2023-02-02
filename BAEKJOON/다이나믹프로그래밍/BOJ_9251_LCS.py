0da14a3abd35532a2063a5d0c5d94bf3d9815a09
ChanYoungHong <cksdud9883@naver.com>
2023-02-02 16:33:32 +0900

    2/2 목 -> 기출 LCS 문제 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_9251_LCS.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_9251_LCS.py
@@ -1,17 +1,23 @@
 import sys
 
-word1 = input()
-word2 = input()
+input = sys.stdin.readline
 
-a, b = len(word1), len(word2)
+word1 = input().rstrip()
+word2 = input().rstrip()
 
-array = [[0] * (b+1) for _ in range(a+1)]
+a,b = len(word1), len(word2)
+
+dp = [[0] * (b+1) for _ in range(a+1)]
 
 for i in range(1, a+1):
     for j in range(1, b+1):
-        if word1[i-1] == word2[j-1]:
-            array[i][j] = array[i-1][j-1] + 1
-        else:
-            array[i][j] = max(array[i][j-1], array[i-1][j])
 
-print(array[-1][-1])
+        if word1[i-1] == word2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
+
+
