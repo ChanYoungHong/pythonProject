2c80161f67887c0f0d6a4b0f98a83592b48d0a50
ChanYoungHong <cksdud9883@naver.com>
2023-02-02 12:15:07 +0900

    2/2 목 -> 기본 수열 문제

--- BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
@@ -4,24 +4,16 @@ input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
+
 dp = [[1] * n for _ in range(2)]
 
-# num = list(map(int, input().split()))
-# d = [[1] * n for _ in range(2)]
+for i in range(n - 1):
 
-for i in range(n-1):
+    if nums[i] >= nums[i + 1]:
+        dp[0][i + 1] = dp[0][i] + 1
 
-    if nums[i] <= nums[i+1]:
-        dp[0][i+1] = dp[0][i] + 1
-    if nums[i] >= nums[i+1]:
-        dp[1][i+1] = dp[1][i] + 1
+    if nums[i] <= nums[i + 1]:
+        dp[1][i + 1] = dp[1][i] + 1
+
 
 print(max(map(max, dp)))
-#
-# for i in range(1,n):
-#     if num[i-1]<=num[i]: #증가할경우
-#         d[0][i] = d[0][i-1]+1
-#     if num[i-1]>=num[i]: #감소할경우
-#         d[1][i] = d[1][i-1]+1
-# print(max(map(max,d)))
-# print(d)
