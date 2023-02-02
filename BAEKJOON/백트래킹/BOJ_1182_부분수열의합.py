d8f92f520ba6a0575da184ad6a1242523a9bb20e
ChanYoungHong <cksdud9883@naver.com>
2023-02-02 11:50:11 +0900

    2/2 목 -> 기본순열 조합 문제

---
+++ BAEKJOON/백트래킹/BOJ_1182_부분수열의합.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+nums = list(map(int, input().split()))
+
+result = []
+cnt = 0
+def dfs(start):
+
+    global cnt
+
+    if sum(result) == m and len(result) > 0:
+        cnt += 1
+
+    for i in range(start, n):
+        result.append(nums[i])
+        dfs(i+1)
+        result.pop()
+
+dfs(0)
+print(cnt)
+
+
