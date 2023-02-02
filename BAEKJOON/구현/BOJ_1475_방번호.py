7968297b3041aeb6f3c9d1803b38a73e0b290f5e
ChanYoungHong <cksdud9883@naver.com>
2023-02-02 14:35:11 +0900

    2/2 목 -> 기본구현 풀이

--- BAEKJOON/구현/BOJ_1475_방번호.py
+++ BAEKJOON/구현/BOJ_1475_방번호.py
@@ -2,23 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-n = input().rstrip()
+word = input().rstrip()
 
-p_set = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-cnt = 1
+array = [0] * 9
 
-new_set = [0] * 10
-
-for i in n:
-
+for i in word:
     idx = int(i)
 
     if idx == 9:
         idx = 6
 
-    new_set[idx] += 1
-new_set[6] = (new_set[6] + 1) // 2
-print(max(new_set))
+    array[idx] += 1
 
+array[6] = (array[6] + 1) // 2
 
-
+print(max(array))
