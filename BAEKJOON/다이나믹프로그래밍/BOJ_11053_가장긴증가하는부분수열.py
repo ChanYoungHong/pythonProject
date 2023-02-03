4d77614e990411298c34b2922f7e7e6ac092d506
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 23:18:51 +0900

    2/3 금 -> dp기본 문제 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -1,17 +1,18 @@
-
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
+
 nums = list(map(int, input().split()))
 
 dp = [1] * (n+1)
 
+
 for i in range(n):
     for j in range(i):
+
         if nums[i] > nums[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
+            dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
-
