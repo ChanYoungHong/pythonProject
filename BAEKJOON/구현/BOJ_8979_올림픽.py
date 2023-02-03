8be0b94f77afbb909c3a2200c265eb9fcf5c6480
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 16:58:01 +0900

    2/3 금 -> 기본구현 복습 완료

--- BAEKJOON/구현/BOJ_8979_올림픽.py
+++ BAEKJOON/구현/BOJ_8979_올림픽.py
@@ -2,29 +2,29 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n, m = map(int, input().rstrip().split())
+
 graph = []
-result = []
-for i in range(n):
-
-    graph = list(map(int, input().split()))
-
-    if graph[0] != m:
-        result.append(graph)
-    else:
-        standard = graph
 
 cnt = 1
-for i in range(len(result)):
-    if result[i][1] > standard[1]:
+standard = []
+for i in range(n):
+    z = list(map(int, input().rstrip().split()))
+
+    if z[0] != m:
+        graph.append(z)
+    else:
+        standard = z
+
+for j in range(len(graph)):
+
+    if graph[j][1] > standard[1]:
         cnt += 1
-
-    elif result[i][1] == standard[1]:
-        if result[i][2] > standard[2]:
+    elif graph[j][1] == standard[1]:
+        if graph[j][2] > standard[2]:
             cnt += 1
-        elif result[i][2] == standard[2]:
-            if result[i][3] > standard[3]:
+        elif graph[j][2] == standard[2]:
+            if graph[j][3] > standard[3]:
                 cnt += 1
-
 
 print(cnt)
