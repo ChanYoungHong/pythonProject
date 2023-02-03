411a712874435e46dbfd4032a51820a53d4df9bd
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 23:09:07 +0900

    2/3 금 -> dp기본 문제 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
@@ -7,13 +7,13 @@ nums = list(map(int, input().split()))
 
 dp = [[1] * n for _ in range(2)]
 
-for i in range(n - 1):
+for i in range(n-1):
 
-    if nums[i] >= nums[i + 1]:
+    if nums[i] >= nums[i+1]:
         dp[0][i + 1] = dp[0][i] + 1
 
-    if nums[i] <= nums[i + 1]:
+    if nums[i] <= nums[i+1]:
         dp[1][i + 1] = dp[1][i] + 1
 
-
+print(dp)
 print(max(map(max, dp)))
