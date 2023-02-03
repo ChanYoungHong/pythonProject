ce8735ba4182696c51e71764c70e7e8797d1b3c2
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 17:39:06 +0900

    2/3 금 -> 기본 그리디, 구현 알고리즘

--- BAEKJOON/그리디/BOJ_1343_폴리오미노.py
+++ BAEKJOON/그리디/BOJ_1343_폴리오미노.py
@@ -1,9 +1,6 @@
-'''
+import sys
 
-1. 시간복잡도 -
-2. 아이디어 -
-
-'''
+input = sys.stdin.readline
 
 word = input()
 
@@ -14,14 +11,3 @@ if 'X' in word:
     print(-1)
 else:
     print(word)
-# temp = 0
-# for i in range(len(word)):
-#     if word[i] == '.':
-#         word[i] = '.'
-#     elif (len(word) % 2) == 0:
-#         if len(word) % 4 == 0:
-#             word[len(word) - 4] = 'A'
-#         elif len(word) % 2 == 0:
-#             word[len(word)-2:0] = 'B'
-#
-# print(''.join(map(str, word)))
