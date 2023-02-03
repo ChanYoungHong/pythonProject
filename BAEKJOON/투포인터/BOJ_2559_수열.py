71067d1d9f9cda2517f070ebf64e5df7e710f617
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 16:07:14 +0900

    2/3 금 -> 누적합 풀이 완료

--- BAEKJOON/투포인터/BOJ_2559_수열.py
+++ BAEKJOON/투포인터/BOJ_2559_수열.py
@@ -2,14 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
-result = []
-result.append(sum(nums[:m]))
+sum = [sum(nums[:m])]
 
 for i in range(n - m):
-    result.append(result[i] - nums[i] + nums[m+i])
+    sum.append(sum[i] - nums[i] + nums[m+i])
 
-print(max(result))
-
+print(max(sum))
