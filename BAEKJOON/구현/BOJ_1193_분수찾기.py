3c670c104285ad4646c8379dc5b9851e9995e992
ChanYoungHong <cksdud9883@naver.com>
2023-02-03 15:17:37 +0900

    2/3 금 -> 기본 구현문제 복습완료

--- BAEKJOON/구현/BOJ_1193_분수찾기.py
+++ BAEKJOON/구현/BOJ_1193_분수찾기.py
@@ -4,35 +4,21 @@ input = sys.stdin.readline
 
 n = int(input())
 
-line = 1
-while n > line:  # 6 > 1
-    n -= line  # 5
-    line += 1  # 2
+line = 0
 
-if line % 2 == 0:  # 0
-    a = n  # a = 5
-    b = line - n + 1  #
-else:
+while n > line:
+    n -= line
+    line += 1
+
+if line % 2 == 0:
+    a = n
+    b = line - n + 1
+
+if line % 2 != 0:
     a = line - n + 1
     b = n
 
-print(a, '/', b, sep='')
+a = str(a)
+b = str(b)
 
-# array = []
-#
-# for i in range(1, n + 1):
-#     for j in range(1, n + 1):
-#         array[i][j].append([str(i) + '/' + str(j)])
-#
-# dx = [0, 1, 1, -1]
-# dy = [0, -1, 0, 1]
-#
-# print(array[1][1])
-# print(array[1][2])
-
-# for i in range(1, n + 1):
-#     for j in range(1, n + 1):
-#         dx = array[i][j] + dx[i]
-#         dy = array[i][j] + dy[i]
-#
-# print(*array[dx][dy])
+print(a+'/'+b)
