d389ea929471a07e8415f0dca2c95b6d440092c0
ChanYoungHong <cksdud9883@naver.com>
2023-02-06 20:33:28 +0900

    2/6 월 -> dp, 3일 전꺼 복습 완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_2491_수열.py
@@ -3,17 +3,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-nums = list(map(int, input().split()))
+num = list(map(int, input().split()))
 
 dp = [[1] * n for _ in range(2)]
 
 for i in range(n-1):
 
-    if nums[i] >= nums[i+1]:
+    if num[i] <= num[i+1]:
         dp[0][i + 1] = dp[0][i] + 1
 
-    if nums[i] <= nums[i+1]:
+    if num[i] >= num[i+1]:
         dp[1][i + 1] = dp[1][i] + 1
 
-print(dp)
 print(max(map(max, dp)))
