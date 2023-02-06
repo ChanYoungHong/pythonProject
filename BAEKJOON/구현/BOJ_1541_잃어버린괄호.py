778cc952e21286a2c748a8eaa30d3ab33b3d151b
ChanYoungHong <cksdud9883@naver.com>
2023-02-06 21:22:18 +0900

    2/6 월 -> 구현문제, 3일 전꺼 복습 완료

--- BAEKJOON/구현/BOJ_1541_잃어버린괄호.py
+++ BAEKJOON/구현/BOJ_1541_잃어버린괄호.py
@@ -1,4 +1,3 @@
-
 import sys
 
 input = sys.stdin.readline
@@ -7,14 +6,18 @@ word = input().rstrip().split('-')
 
 result = []
 for i in word:
+
     sum = 0
     new_word = i.split('+')
+
     for j in new_word:
+
         sum += int(j)
     result.append(sum)
 
 z = result[0]
 
-for i in range(1, len(result)):
-    z -= result[i]
+for k in range(1, len(result)):
+    z -= result[k]
+
 print(z)
