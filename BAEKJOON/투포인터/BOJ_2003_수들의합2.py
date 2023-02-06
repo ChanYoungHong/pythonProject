6b00911db7bba23638801df9373eb62f445a3958
ChanYoungHong <cksdud9883@naver.com>
2023-02-06 17:07:43 +0900

    2/6 월 -> 투 포인터 문제, 7일 전꺼 복습 완료

--- BAEKJOON/투포인터/BOJ_2003_수들의합2.py
+++ BAEKJOON/투포인터/BOJ_2003_수들의합2.py
@@ -1,3 +1,9 @@
+'''
+
+n < 10,000 보다 작으니깐 투 포인터가 적합 함
+
+'''
+
 import sys
 
 input = sys.stdin.readline
@@ -5,19 +11,20 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
-end = 0
 inter_sum = 0
+end = 0
 cnt = 0
 
-for start in range(n):
+for i in range(n):
 
     while inter_sum < m and end < n:
+
         inter_sum += nums[end]
         end += 1
 
     if inter_sum == m:
         cnt += 1
 
-    inter_sum -= nums[start]
+    inter_sum -= nums[i]
 
 print(cnt)
