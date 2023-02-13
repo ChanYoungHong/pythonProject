8242784d711d7564176e17cf405d289af37f19f5
ChanYoungHong <cksdud9883@naver.com>
2023-02-13 21:29:01 +0900

    2/13 월 -> 가장 긴 증가하는 부분수열, 두 번째 for문 매개변수 뭐 넣을지 고민하기

--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -7,10 +7,11 @@ nums = list(map(int, input().split()))
 
 dp = [1] * n
 
-for i in range(n):
-    for j in range(i):
+for i in range(n): # 3
+    for j in range(i): # 0 1 2
 
         if nums[i] > nums[j]:
+            print(nums[i], nums[j])
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
