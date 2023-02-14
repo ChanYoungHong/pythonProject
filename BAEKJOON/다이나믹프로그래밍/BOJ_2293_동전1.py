0adf05a144b06782ca18ea0760375709f5d22222
ChanYoungHong <cksdud9883@naver.com>
2023-02-14 16:44:07 +0900

    2/13 화 -> dp문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_2293_동전1.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_2293_동전1.py
@@ -2,23 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-
+n, m = map(int,input().split())
 dp = [0] * (m+1)
+dp[0] = 1
 
 coins = []
 for _ in range(n):
     a = int(input())
     coins.append(a)
 
-dp[0] = 1
-for i in coins:
-    for j in range(i, m+1):
-        if j-i >= 0:
-            dp[j] += dp[j-i]
+for coin in coins:
+    for i in range(coin, m+1):
+
+        if i - coin >= 0:
+            dp[i] += dp[i-coin]
 
 print(dp[m])
-
-
-
-
