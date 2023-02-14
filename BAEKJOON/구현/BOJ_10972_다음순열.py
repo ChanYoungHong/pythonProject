34e1a36ed65cad80556a24ce39eeb67bfc8dfea2
ChanYoungHong <cksdud9883@naver.com>
2023-02-14 19:40:28 +0900

    2/13 화 -> 문자열 복습

--- BAEKJOON/구현/BOJ_10972_다음순열.py
+++ BAEKJOON/구현/BOJ_10972_다음순열.py
@@ -1,4 +1,3 @@
-
 import sys
 
 input = sys.stdin.readline
@@ -6,37 +5,15 @@ input = sys.stdin.readline
 n = int(input())
 num = list(map(int, input().split()))
 
-for i in range(n-1, 0, -1):
+for i in range(n - 1, 0, -1):
 
-    if num[i] > num[i-1]:
-        for j in range(n-1, 0, -1):
-            if num[i-1] < num[j]:
-                num[i-1], num[j] = num[j], num[i-1]
+    if num[i] > num[i - 1]:
+        for j in range(n - 1, 0,-1):
+
+            if num[i - 1] < num[j]:
+                num[i - 1], num[j] = num[j], num[i - 1]
                 num = num[:i] + sorted(num[i:])
                 print(*num)
                 exit(0)
 
 print(-1)
-import sys
-
-input = sys.stdin.readline
-
-n = int(input())
-num = list(map(int, input().split()))
-
-for i in range(n-1, 0, -1):
-
-    if num[i] > num[i-1]:
-        for j in range(n-1, 0, -1):
-            if num[i-1] < num[j]:
-                num[i-1], num[j] = num[j], num[i-1]
-                num = num[:i] + sorted(num[i:])
-                print(*num)
-                exit(0)
-
-print(-1)
-
-
-
-
-
