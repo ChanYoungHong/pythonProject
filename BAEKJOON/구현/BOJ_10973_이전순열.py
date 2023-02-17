1322f0e644aeeb9687695a9598de0b0c0233d058
ChanYoungHong <cksdud9883@naver.com>
2023-02-17 20:08:54 +0900

    2/17 금 -> 기본 구현문제 복습

--- BAEKJOON/구현/BOJ_10973_이전순열.py
+++ BAEKJOON/구현/BOJ_10973_이전순열.py
@@ -2,32 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-a = list(map(int, input().split()))
+n = int(input())
+num = list(map(int, input().split()))
 
-for i in range(N-1, 0, -1):
-    if a[i] < a[i-1]: # 1 < 2
-        x, y = i-1, i # 4 5
-        for j in range(N-1, 0, -1):
-            if a[j] < a[x]: # a[5] < a[4]
-                a[j], a[x] = a[x], a[j]
-                a = a[:i] + sorted(a[i:], reverse=True)
-                print(*a)
-                exit(0)
+for i in range(n - 1, 0, -1):
+
+    if num[i] < num[i - 1]:
+
+        for j in range(n - 1, 0, -1):
+
+            if num[i - 1] > num[j]:
+                num[i - 1], num[j] = num[j], num[i - 1]
+                result = num[:i] + sorted(num[i:], reverse = True)
+                print(*result)
+                sys.exit(0)
+
 print(-1)
-
-# result = []
-# answer = []
-# def dfs():
-#
-#     if len(result) == n:
-#         print(*result)
-#         return
-#
-#     for i in range(1, n+1):
-#         if i not in result:
-#             result.append(i)
-#             dfs()
-#             result.pop()
-#
-# dfs()
