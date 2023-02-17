7025d785f24d2a6301df1c732b7d277d28b91277
ChanYoungHong <cksdud9883@naver.com>
2023-02-17 21:56:35 +0900

    2/17 금 -> 2차원 dp 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
@@ -5,11 +5,11 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0] * (m+1) for _ in range(n+1)]
+dp = [[0] * (m + 1) for i in range(n + 1)]
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
+for i in range(n):
+    for j in range(m):
 
-        dp[i][j] += graph[i-1][j-1] + max(dp[i-1][j], dp[i][j-1])
+        dp[i][j] = graph[i][j] + max(dp[i - 1][j], dp[i][j - 1])
 
-print(max(map(max, dp)))
+print(dp[n-1][m-1])
