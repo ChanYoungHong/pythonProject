b65c172499cd173924c7092c2b7e9cd65b5e6a35
ChanYoungHong <cksdud9883@naver.com>
2023-02-22 16:50:20 +0900

    2/22 수 -> 2차원 배열, dp 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
@@ -2,14 +2,14 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0] * (m + 1) for i in range(n + 1)]
+dp = [[0] * (m+1) for _ in range(n+1)]
 
 for i in range(n):
     for j in range(m):
 
-        dp[i][j] = graph[i][j] + max(dp[i - 1][j], dp[i][j - 1])
+        dp[i][j] = graph[i][j] + max(dp[i-1][j], dp[i][j-1])
 
 print(dp[n-1][m-1])
