13763794da1f447ea54191c9dc550bb219996856
ChanYoungHong <cksdud9883@naver.com>
2023-02-23 14:37:01 +0900

    2/23 목 -> 구현 문제 복습

--- BAEKJOON/구현/BOJ_8979_올림픽.py
+++ BAEKJOON/구현/BOJ_8979_올림픽.py
@@ -2,29 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split())
+n, m = map(int, input().split())
 
-graph = []
+ranking = []
+standard = []
+for _ in range(n):
+
+    arr = list(map(int, input().rstrip().split()))
+
+    if arr[0] != m:
+        ranking.append(arr)
+    else:
+        standard = arr
+        # standard.append(arr)
 
 cnt = 1
-standard = []
-for i in range(n):
-    z = list(map(int, input().rstrip().split()))
 
-    if z[0] != m:
-        graph.append(z)
-    else:
-        standard = z
+for j in range(len(ranking)):
 
-for j in range(len(graph)):
-
-    if graph[j][1] > standard[1]:
+    if ranking[j][1] > standard[1]:
         cnt += 1
-    elif graph[j][1] == standard[1]:
-        if graph[j][2] > standard[2]:
+    elif ranking[j][1] == standard[1]:
+        if ranking[j][2] > standard[2]:
             cnt += 1
-        elif graph[j][2] == standard[2]:
-            if graph[j][3] > standard[3]:
+        elif ranking[j][2] == standard[2]:
+            if ranking[j][3] > standard[3]:
                 cnt += 1
 
 print(cnt)
