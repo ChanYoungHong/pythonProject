9fa036b3da7ac2965ef87583dbdd24880a9bbdf7
ChanYoungHong <cksdud9883@naver.com>
2023-02-23 14:53:15 +0900

    2/23 목 -> dp, 1 2 3 더하기 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_9095_123더하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_9095_123더하기.py
@@ -3,16 +3,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = [0] * 11
 
+dp = [0] * 12
 
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in range(4, 11):
+for i in range(4, 12):
     dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
 for i in range(n):
+    # a = int(input)
     print(dp[int(input())])
 
