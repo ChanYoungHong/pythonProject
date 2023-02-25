470c0944a11a2a08aec0bfcd0bb8fd81ed836eaf
ChanYoungHong <cksdud9883@naver.com>
2023-02-25 16:54:03 +0900

    2/25 토 -> 이진탐색 문제 복습완료

--- BAEKJOON/이진탐색/BOJ_1756_피자굽기.py
+++ BAEKJOON/이진탐색/BOJ_1756_피자굽기.py
@@ -2,25 +2,27 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int, input().split())
 oven = list(map(int, input().split()))
 pizza = list(map(int, input().split()))
+
 
 for i in range(n-1):
 
     if oven[i] < oven[i+1]:
         oven[i+1] = oven[i]
 
-
 dep = 0
+for i in range(n-1, -1,-1):
 
-for j in range(n-1, 0, -1):
+    if oven[i] < pizza[dep]:
+        continue
 
-    if oven[j] >= pizza[dep]:
+    if oven[i] >= pizza[dep]:
         dep += 1
 
-    if dep >= m:
-        print(j+1)
+    if dep == m:
+        print(i+1)
         sys.exit()
 
 print(0)
