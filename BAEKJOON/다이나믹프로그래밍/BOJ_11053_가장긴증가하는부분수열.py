426aa76c2721e6c6bc42d4d0e8ce114a79a17356
ChanYoungHong <cksdud9883@naver.com>
2023-02-25 17:02:00 +0900

    2/25 토 -> dp 문제 복습완료

--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -1,3 +1,10 @@
+'''
+
+1. 시간복잡도 -> 1 < < 1000 사이니깐
+
+'''
+
+
 import sys
 
 input = sys.stdin.readline
@@ -5,13 +12,13 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 
-dp = [1] * n
+dp = [1 for _ in range(n)]
 
-for i in range(n): # 3
-    for j in range(i): # 0 1 2
+for i in range(n):
+    for j in range(i):
 
         if nums[i] > nums[j]:
-            print(nums[i], nums[j])
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
+
