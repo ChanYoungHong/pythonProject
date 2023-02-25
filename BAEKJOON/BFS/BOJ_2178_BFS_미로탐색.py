ee1edfc5b2dd1571f6cdfa909026a5783aff8c9b
ChanYoungHong <cksdud9883@naver.com>
2023-02-25 13:06:41 +0900

    2/25 토 -> bfs 복습

--- BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
+++ BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
@@ -1,35 +1,36 @@
+import sys
 from collections import deque
 
-a, b = map(int, input().split())
+input = sys.stdin.readline
 
-graph = [list(map(int, input())) for _ in range(a)]
+n, m = map(int, input().split())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def bfs(x,y):
+
+def bfs(x, y):
 
     q = deque()
-    q.append((x,y))
+    q.append((x, y))
 
     while q:
 
-        x,y = q.popleft()
+        x, y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= a or ny >= b:
-                continue
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
 
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx,ny))
-
-    return graph[a-1][b-1]
+    return graph[-1][-1]
 
 print(bfs(0,0))
+
+
+
