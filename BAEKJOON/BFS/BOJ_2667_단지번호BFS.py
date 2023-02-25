968897c400fd740d27899dd5b1925e07e064ac0d
ChanYoungHong <cksdud9883@naver.com>
2023-02-25 15:03:11 +0900

    2/25 토 -> bfs 복습

--- BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
+++ BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
@@ -1,21 +1,28 @@
 import sys
 from collections import deque
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+input = sys.stdin.readline
 
 n = int(input())
-graph = [list(map(int, input())) for _ in range(n)]
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-result = []
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+res = []
+
 def bfs(x,y):
-    home = 1
+
+    cnt = 1
     q = deque()
     q.append((x,y))
-    graph[x][y] = 0
+
     while q:
 
         x,y = q.popleft()
+        graph[x][y] = 0
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -24,19 +31,19 @@ def bfs(x,y):
                 if graph[nx][ny] == 1:
                     graph[nx][ny] = 0
                     q.append((nx,ny))
-                    home += 1
+                    cnt += 1
 
-    result.append(home)
+    res.append(cnt)
 
-cnt = 0
+house = 0
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
             bfs(i,j)
-            cnt += 1
+            house += 1
 
-result.sort()
+print(house)
+res.sort()
 
-print(cnt)
-for i in result:
+for i in res:
     print(i)
