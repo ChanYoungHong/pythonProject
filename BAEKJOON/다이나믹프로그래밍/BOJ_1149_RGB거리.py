09136f204bc124db1cdb465cab0cb83caddbf855
ChanYoungHong <cksdud9883@naver.com>
2023-02-27 17:10:35 +0900

    2/27 월 -> dp 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_1149_RGB거리.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_1149_RGB거리.py
@@ -1,17 +1,13 @@
 import sys
 
-input = sys.stdin.readline
-
 n = int(input())
+
 graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
-for i in range(1, n):
-    graph[i][0] = min(graph[i - 1][1], graph[i - 1][2]) + graph[i][0]
-    graph[i][1] = min(graph[i - 1][0], graph[i - 1][2]) + graph[i][1]
-    graph[i][2] = min(graph[i - 1][0], graph[i - 1][1]) + graph[i][2]
+for i in range(1, n): # 0 1 2
 
-print(min(graph[n - 1]))
+    graph[i][0] = min(graph[i-1][1], graph[i-1][2]) + graph[i][0]
+    graph[i][1] = min(graph[i-1][0], graph[i-1][2]) + graph[i][1]
+    graph[i][2] = min(graph[i-1][0], graph[i-1][1]) + graph[i][2]
 
-
-
-
+print(min(graph[n-1]))
