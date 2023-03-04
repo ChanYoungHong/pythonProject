00cee97b58f641e0295ca02c22e9dc95678f8e90
ChanYoungHong <cksdud9883@naver.com>
2023-03-04 18:58:45 +0900

    3월 4일 토 -> 투 포인터 문제

--- BAEKJOON/투포인터/BOJ_2559_수열.py
+++ BAEKJOON/투포인터/BOJ_2559_수열.py
@@ -5,10 +5,9 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
-dp = [sum(nums[:m])]
+temp = [sum(nums[:m])]
 
-for i in range(n-m):
+for j in range(n - m):  # 0 1 2 3 4 5 6 7 8 9
+    temp.append(temp[j] - nums[j] + nums[j + m])
 
-    dp.append(dp[i] - nums[i] + nums[m+i])
-
-print(max(dp))
+print(max(temp))
