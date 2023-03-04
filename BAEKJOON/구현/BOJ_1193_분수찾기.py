26efc714b0f3e4322321c62ba9fb5de84f38d190
ChanYoungHong <cksdud9883@naver.com>
2023-03-04 17:13:50 +0900

    3월 4일 토 -> 기본 구현문제 복습

--- BAEKJOON/구현/BOJ_1193_분수찾기.py
+++ BAEKJOON/구현/BOJ_1193_분수찾기.py
@@ -5,16 +5,18 @@ input = sys.stdin.readline
 n = int(input())
 
 line = 0
-
+''' n이 line보다 더 클 때까지만 반복해라'''
+''' 즉, line이 더 클 땐 빠져나감'''
 while n > line:
     n -= line
     line += 1
 
+print(n, line)
+
 if line % 2 == 0:
     a = n
     b = line - n + 1
-
-if line % 2 != 0:
+elif line % 2 == 1:
     a = line - n + 1
     b = n
 
@@ -22,3 +24,5 @@ a = str(a)
 b = str(b)
 
 print(a+'/'+b)
+
+
