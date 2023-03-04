097f6f5831850fef5579a56ead41b9348dbd1564
ChanYoungHong <cksdud9883@naver.com>
2023-03-04 17:27:32 +0900

    3월 4일 토 -> 투 포인터 문제

--- BAEKJOON/투포인터/BOJ_2003_수들의합2.py
+++ BAEKJOON/투포인터/BOJ_2003_수들의합2.py
@@ -1,10 +1,3 @@
-'''
-
-1. 시간복잡도 -> 1 < n < 10,000 사이이니깐 투 포인터, DP 등을 사용해야 함
-2.
-
-'''
-
 import sys
 
 input = sys.stdin.readline
@@ -12,18 +5,20 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
-inter_num = 0
 cnt = 0
+inter_num = 0
 end = 0
 
 for i in range(n):
 
     while inter_num < m and end < n:
+
         inter_num += nums[end]
         end += 1
 
     if inter_num == m:
         cnt += 1
+
     inter_num -= nums[i]
 
 print(cnt)
