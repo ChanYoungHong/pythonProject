14f7ca054cd7aacb2e169776086109d2ec22b3b1
ChanYoungHong <cksdud9883@naver.com>
2023-03-06 16:32:37 +0900

    3월 6일 월-> 구현 문제

--- BAEKJOON/구현/BOJ_7568_덩치.py
+++ BAEKJOON/구현/BOJ_7568_덩치.py
@@ -4,17 +4,18 @@ input = sys.stdin.readline
 
 n = int(input())
 
-result = []
+height = []
 for _ in range(n):
-    a,b = map(int, input().split())
-    result.append((a,b))
+    a, b = map(int, input().split())
+    height.append((a,b))
 
 answer = []
 for i in range(n):
     cnt = 0
     for j in range(n):
-        if result[i][0] < result[j][0] and result[i][1] < result[j][1]:
-            cnt += 1
-    answer.append(cnt+1)
 
-print(*answer)
+        if height[i][0] < height[j][0] and height[i][1] < height[j][1]:
+            cnt += 1
+    answer.append(cnt + 1)
+
+print(answer)
