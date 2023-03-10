c38d775c4312b8b7dd21af903e818e3132fe8ca8
ChanYoungHong <cksdud9883@naver.com>
2023-03-10 15:47:31 +0900

    3월 10일 금 -> dfs로 복습 완료

--- BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
+++ BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
@@ -1,95 +1,42 @@
-# from collections import deque
-# import sys
-#
-# input = sys.stdin.readline
-#
-# n = int(input().rstrip())
-# graph = [list(map(int, input().rstrip())) for _ in range(n)]
-#
-# # for _ in range(n):
-# #     graph.append(list(map(int, input().rstrip())))
-#
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-#
-# visited = [[False] * n for _ in range(n)]
-# result = []
-# cnt2 = 0
-#
-# def bfs(x, y):
-#     cnt = 1
-#     q = deque()
-#     q.append((x, y))
-#     graph[x][y] = 0
-#     while q:
-#
-#         x, y = q.popleft()
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-#             if nx < 0 or ny < 0 or nx >= n or ny >= n:
-#                 continue
-#
-#             if graph[nx][ny] == 0:
-#                 continue
-#
-#             if graph[nx][ny] == 1:
-#                 graph[nx][ny] = 0
-#                 q.append((nx, ny))
-#                 cnt += 1
-#     result.append(cnt)
-#
-# for i in range(n):
-#     for j in range(n):
-#         if graph[i][j] == 1:
-#             bfs(i, j)
-#             cnt2 += 1
-#
-# result.sort()
-#
-# print(cnt2)
-# for i in result:
-#     print(i)
-
-
 import sys
 
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
+n = int(input())
 
 dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-answer = []
+dy = [0,0,1,-1]
 
-def dfs(x, y):
-
-    if x < 0 or y < 0 or x >= n or y >= n:
-        return False
-
-    if graph[x][y] == 1:
-        global cnt
-        cnt += 1
-        graph[x][y] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
-        return True
-
+visited = [[False] * n for _ in range(n)]
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
 cnt = 0
-result = 0
+def dfs(x,y):
 
+    global cnt
+    visited[x][y] = True
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < n:
+            if visited[nx][ny] == False and graph[nx][ny] == 1:
+                dfs(nx,ny)
+                cnt += 1
+
+    return cnt
+
+res = []
+house = 0
 for i in range(n):
     for j in range(n):
-        if dfs(i, j) == True:
-            answer.append(cnt)
-            result += 1
-            cnt = 0
 
-answer.sort()
-print(result)
-for i in answer:
+        if visited[i][j] == False and graph[i][j] == 1:
+            cnt = 1
+            dfs(i,j)
+            res.append(cnt)
+
+print(len(res))
+for i in sorted(res):
     print(i)
