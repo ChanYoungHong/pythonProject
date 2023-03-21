da0a89612627461fb1df949c43466e3d98796851
ChanYoungHong <cksdud9883@naver.com>
2023-03-21 21:39:45 +0900

    3월 21일 화 -> 이진탐색 답 보고 품

--- BAEKJOON/이진탐색/BOJ_2512_예산.py
+++ BAEKJOON/이진탐색/BOJ_2512_예산.py
@@ -1,3 +1,12 @@
+'''
+1. 수도코드 -> 일단 n만큼 나눠서 나눈 기준의 수로
+2. 기준 수보다 크면 따로 배열에 담아서 처리
+3. 무슨 기준으로 저 답을 찾을 수 있나 ??
+4. 기준보다 큰 수들을 남겨놓고 그것의 평균을 구하면 답이 나오지 않을까..
+5.
+
+'''
+
 import sys
 
 input = sys.stdin.readline
@@ -5,24 +14,42 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 m = int(input())
-temp = []
 start = 1
 end = max(nums)
 
-
-standard = m // n
-
-for i in nums:
-    if i >= standard:
-        temp.append(i)
-
-
+# nums.sort()
+# print(nums)
+#
+# standard = m // n
+#
+# for i in nums:
+#     if i < standard:
+#         m -= i
+# print(m)
+#
+#
+# while start <= end:
+#     mid = (start + end) // 2
+#
+#
+# '''127일 찾아야 하는데 무슨 기준으로 찾음 ???? '''
+#     # if sum(nums)
 
 while start <= end:
+
     mid = (start + end) // 2
+    total = 0
 
+    for i in nums:
+        if i > mid: # 110 > 75
+            total += mid
+        else:
+            total += i
 
+    if total <= m:
+        start = mid + 1
+    else:
+        end = mid - 1
 
-    if sum(nums)
-
+print(end)
 
