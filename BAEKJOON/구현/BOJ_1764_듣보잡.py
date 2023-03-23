3e873a78404e497e13e071c5845835cf2f13693d
ChanYoungHong <cksdud9883@naver.com>
2023-03-23 10:34:58 +0900

    3월 23일 목 -> 22일 문제 복습

--- BAEKJOON/구현/BOJ_1764_듣보잡.py
+++ BAEKJOON/구현/BOJ_1764_듣보잡.py
@@ -2,19 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().rstrip().split())
+n, m = map(int, input().split())
 
-a = set()
-
+a = [ ]
 for i in range(n):
-    a.add(input().rstrip())
+    a.append(input().rstrip())
 
-b = set()
-for i in range(m):
-    b.add(input().rstrip())
+b = []
+for j in range(m):
+    b.append(input().rstrip())
 
-same = sorted(set(a&b))
+a.sort()
+b.sort()
 
-print(len(same))
-for i in same:
+def bs(start, end, target):
+
+    while start <= end:
+        mid = (start + end) // 2
+        if a[mid] == target:
+            return True
+        elif a[mid] < target:
+            start = mid + 1
+        elif a[mid] > target:
+            end = mid - 1
+    return False
+
+
+result = []
+for i in b:
+    if bs(0, len(a)-1, i):
+        result.append(i)
+
+result.sort()
+print(len(result))
+for i in result:
     print(i)
