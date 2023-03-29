8abf871e80d261b700752b9c2624077fa9d65f39
ChanYoungHong <cksdud9883@naver.com>
2023-03-29 17:03:55 +0900

    파도반수열 -> 스터디 문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_9461_파도반수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_9461_파도반수열.py
@@ -2,19 +2,25 @@ import sys
 
 input = sys.stdin.readline
 
-dp = [0 for i in range(101)]
+t = int(input().rstrip())
+
+
+dp = [0] * 100
 
 dp[1] = 1
 dp[2] = 1
 dp[3] = 1
+dp[4] = 2
 
-for i in range(0, 98):
-    dp[i+3] = dp[i] + dp[i+1]
+for i in range(4, 99):
+    dp[i] = dp[i-2] + dp[i-3]
 
-t = int(input())
+# print(dp[t])
+
 for i in range(t):
     n = int(input())
     print(dp[n])
+
 
 '''
 
