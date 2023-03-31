9b26c67798df5f63ad26967c74e3713536b1ca22
ChanYoungHong <cksdud9883@naver.com>
2023-03-31 17:08:39 +0900

    스터디 dp 문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_15988_123더하기3.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_15988_123더하기3.py
@@ -2,9 +2,10 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input().rstrip())
+t = int(input())
 
-dp = [0] * 1000001
+
+dp =  [0] * 1000001
 
 dp[1] = 1
 dp[2] = 2
@@ -13,8 +14,6 @@ dp[3] = 4
 for i in range(4, 1000001):
     dp[i] = (dp[i-3] + dp[i-2] + dp[i-1]) % 1000000009
 
-for _ in range(n):
 
-    print(dp[int(input())] % 1000000009)
-
-# print(dp[n])
+for _ in range(t):
+    print(dp[int(input())])
