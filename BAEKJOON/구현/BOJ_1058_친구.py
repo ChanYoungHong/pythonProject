68e12437b4c2c21e3fa63f4435acc2ed4d36a4a2
ChanYoungHong <cksdud9883@naver.com>
2023-04-08 19:40:28 +0900

    2/21 문제 복완

--- BAEKJOON/구현/BOJ_1058_친구.py
+++ BAEKJOON/구현/BOJ_1058_친구.py
@@ -11,11 +11,6 @@ INF = sys.maxsize
 
 temp = [[0] * n for _ in range(n)]
 
-# for i in range(n):
-#     for j in range(n):
-#
-#         if i == j:
-#             board[]
 
 for i in range(n):
     for j in range(n):
