c8ddda7be6c721ae2e1140de09792c383d628620
ChanYoungHong <cksdud9883@naver.com>
2023-04-18 15:59:56 +0900

    4/18일 스터디문제 복습

--- BAEKJOON/DFS/BOJ_3187_양치기꿍.py
+++ BAEKJOON/DFS/BOJ_3187_양치기꿍.py
@@ -19,26 +19,25 @@ if 절로
 
 '''
 
-n, m = map(int, input().split())
-board = [list(input().rstrip()) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
-
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+'''
+양 -> k
+늑대 -> v
 
-
+'''
 def dfs(x,y):
 
-    global wolf, sheep
+    global sheep, wolf
     visited[x][y] = 1
 
+    if board[x][y] == 'k':
+        sheep += 1
 
     if board[x][y] == 'v':
         wolf += 1
 
-    if board[x][y] == 'k':
-        sheep += 1
 
     for i in range(4):
 
@@ -46,19 +45,21 @@ def dfs(x,y):
         ny = y + dy[i]
 
         if 0 <= nx < n and 0 <= ny < m:
-            if visited[nx][ny] == False and board[nx][ny] != '#':
+            if board[nx][ny] != '#' and visited[nx][ny] == False:
                 dfs(nx,ny)
 
 
-ans1 = 0
-ans2 = 0
+n,m = map(int, input().split())
+board = [list(input()) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
 wolf, sheep = 0,0
+wCnt, sCnt = 0,0
 
 for i in range(n):
     for j in range(m):
 
-        if board[i][j] != '#' and visited[i][j] == False:
+        if visited[i][j] == False and board[i][j] != '#':
 
             dfs(i,j)
 
@@ -67,13 +68,16 @@ for i in range(n):
             else:
                 wolf = 0
 
-            ans1 += sheep
-            ans2 += wolf
+            sCnt += sheep
+            wCnt += wolf
 
             sheep = 0
             wolf = 0
 
-print(ans1, ans2)
+print(sCnt, wCnt)
+
+
+
 
 
 
