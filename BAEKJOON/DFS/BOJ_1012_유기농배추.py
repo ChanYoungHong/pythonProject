4c6897ab924161328f55411804f0f56358340618
ChanYoungHong <cksdud9883@naver.com>
2023-04-21 17:04:55 +0900

    4달 전 문제 복습

--- BAEKJOON/DFS/BOJ_1012_유기농배추.py
+++ BAEKJOON/DFS/BOJ_1012_유기농배추.py
@@ -1,53 +1,64 @@
-'''
-1. 시간복잡도 - 아무렇게나 구현해도 상관이 없음
-
-'''
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
-test = int(input())
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+'''
+<point>
 
+1. 배추와 배추 사이를 이동 할 수 있음
+2. 최소한 필요한 지렁이 수를 구하여라
+3. 0은 배추가 심어져 있지 않은 땅, 1은 배추가 심어져 있는 땅
 
-def bfs(maps, x, y):
+'''
+
+t = int(input())
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+
     q = deque()
-    q.append((x, y))
-    maps[x][y] = 0
+    q.append((x,y))
 
     while q:
-        a, b = q.popleft()
+
+        x,y = q.popleft()
+
         for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= m or ny >= n:
-                continue
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = 0
-                q.append((nx, ny))
-    return
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1:
+                board[nx][ny] = 0
+                q.append((nx,ny))
 
-
-for _ in range(test):
+for i in range(t):
     cnt = 0
-    m, n, k = map(int, input().split())
-    maps = [[0] * n for _ in range(m)]
+    m,n, cab = map(int, input().rstrip().split())
+    board = [[0] * m for _ in range(n)]
 
-    print(maps)
-    for i in range(k):
-        a, b = map(int, input().split())
-        maps[a][b] = 1
+    # m 은 가로,  n은 세로
+    for j in range(cab):
+        a,b = map(int, input().split())
+        board[b][a] = 1
 
-    for x in range(m):
-        for y in range(n):
-            if maps[x][y] == 1:
-                bfs(maps, x, y)
+        # if board[a][b] == 0:
+        #     board[a][b] = 1
+        #     q.append((a,b))
+
+    for z in range(n):
+        for k in range(m):
+
+            if board[z][k] == 1:
+                bfs(z,k)
                 cnt += 1
 
     print(cnt)
+
+
+
+
 
