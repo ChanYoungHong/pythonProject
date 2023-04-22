365f89cbc4a0df5fd513fb9a8963747cab40cff5
ChanYoungHong <cksdud9883@naver.com>
2023-04-22 13:55:07 +0900

    스터디 문제 풀이

--- BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
+++ BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
@@ -1,16 +1,24 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
+
+
+
+
+
 n = int(input())
+board = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+
+# board = [list(map(int, input()))]
 
 dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+dy = [0,0,-1,1]
 
-visited = [[False] * n for _ in range(n)]
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
-cnt = 0
+
 def dfs(x,y):
 
     global cnt
@@ -21,22 +29,22 @@ def dfs(x,y):
         ny = y + dy[i]
 
         if 0 <= nx < n and 0 <= ny < n:
-            if visited[nx][ny] == False and graph[nx][ny] == 1:
+            if visited[nx][ny] == False and board[nx][ny] == 1:
                 dfs(nx,ny)
                 cnt += 1
 
-    return cnt
 
+cnt = 0
 res = []
-house = 0
 for i in range(n):
     for j in range(n):
 
-        if visited[i][j] == False and graph[i][j] == 1:
+        if board[i][j] == 1 and visited[i][j] == False:
             cnt = 1
             dfs(i,j)
             res.append(cnt)
 
+
 print(len(res))
-for i in sorted(res):
+for i in res:
     print(i)
