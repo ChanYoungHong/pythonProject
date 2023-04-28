78b57b6c26bea7ab37bdc03cafbb8a0e2df2bfb3
ChanYoungHong <cksdud9883@naver.com>
2023-04-28 15:12:36 +0900

    스터디 문제 복습완료

--- BAEKJOON/BFS/BOJ_1743_음식물피하기.py
+++ BAEKJOON/BFS/BOJ_1743_음식물피하기.py
@@ -1,43 +1,53 @@
 import sys
 sys.setrecursionlimit(10000)
-
 input = sys.stdin.readline
+
+n,m, cn = map(int, input().split())
+
+graph = [[0] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
+
 
 def dfs(x,y):
 
     global cnt
 
     for i in range(4):
-
         nx = x + dx[i]
         ny = y + dy[i]
 
         if 0 <= nx < n and 0 <= ny < m:
-            if board[nx][ny] == '#':
-                board[nx][ny] = 0
+            if graph[nx][ny] == '#':
+                graph[nx][ny] = 0
                 dfs(nx,ny)
                 cnt += 1
 
 
-res = []
-n,m,k = map(int, input().split())
-cnt = 0
-board = [[0] * (m)for _ in range(n)]
-for _ in range(k):
-    a,b = map(int, input().split())
-    board[a-1][b-1] = '#'
+for i in range(cn):
 
+    a,b = map(int, input().split())
+    graph[a-1][b-1] = '#'
+
+res = 0
+cnt = 0
 for i in range(n):
     for j in range(m):
-        if board[i][j] == '#':
+
+        if graph[i][j] == '#':
             cnt = 0
             dfs(i,j)
-            res.append(cnt)
+            res = max(res, cnt)
 
-print(max(res))
+print(res)
+
+
+
+
+
+
 
 
 
