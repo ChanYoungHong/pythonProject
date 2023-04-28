35d0548a7ea47b26999588735e974a13b5a227ec
ChanYoungHong <cksdud9883@naver.com>
2023-04-28 16:19:45 +0900

    스터디 문제 복습완료

--- BAEKJOON/DFS/BOJ_10026_적록색약.py
+++ BAEKJOON/DFS/BOJ_10026_적록색약.py
@@ -1,66 +1,67 @@
 import sys
+from collections import deque
 
 input = sys.stdin.readline
-sys.setrecursionlimit(1000000)
-
-'''
-1. 궁금한 점 방문 했는지 확인을 해야할까 ?? 굳이 해야하나 ?
-2. 
-'''
 
 n = int(input())
-board = [list(input().rstrip()) for _ in range(n)]
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
+graph = [list(input().rstrip()) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
 
-def dfs(x,y):
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    visited[x][y] = True
-    current_color = board[x][y]
 
-    for i in range(4):
+def bfs(x, y):
 
-        nx = x + dx[i]
-        ny = y + dy[i]
+    q = deque()
+    q.append((x, y))
 
-        if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-            if current_color == board[nx][ny]:
-                dfs(nx,ny)
+    global cnt1, cnt2
+    current_color = graph[x][y]
+
+    while q:
+
+        x, y = q.popleft()
+
+        for i in range(4):
+
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                if visited[nx][ny] == False and current_color == graph[nx][ny]:
+                    visited[nx][ny] = True
+                    q.append((nx, ny))
 
 
 cnt1 = 0
 cnt2 = 0
+
 res = []
 
 for i in range(n):
     for j in range(n):
 
-        if visited[i][j] == False:
-            # board[i][j] = True
-            dfs(i,j)
+        if not visited[i][j]:
+            bfs(i, j)
+            res.append(cnt1)
             cnt1 += 1
 
-res.append(cnt1)
+for i in range(n):
+    for j in range(n):
+
+        if graph[i][j] == 'G':
+            graph[i][j] = 'R'
 
 visited = [[False] * n for _ in range(n)]
 
-for i in range(n):
-    for j in range(n):
-
-        if board[i][j] == 'G':
-            board[i][j] = 'R'
 
 for i in range(n):
     for j in range(n):
 
-        if visited[i][j] == False:
-            # board[i][j] = True
-            dfs(i,j)
+        if not visited[i][j]:
+            bfs(i,j)
+            res.append(cnt2)
             cnt2 += 1
-res.append(cnt2)
 
-print(*res)
-
+print(cnt1, cnt2)
