ff70c860e4add11de1bf44cfe907b1e29f2c3743
ChanYoungHong <cksdud9883@naver.com>
2023-04-30 12:23:05 +0900

    4/29일 못 풀었던 문제 복습하기

--- BAEKJOON/구현/BOJ_2439_별찍기2.py
+++ BAEKJOON/구현/BOJ_2439_별찍기2.py
@@ -4,14 +4,6 @@ input = sys.stdin.readline
 
 n = int(input())
 
-# for i in range(n):
-#     for j in range(n-1, 3-i, -1): # 0 ~ 4:
-#         print("i = ", i)
-#         print("j = ", j)
-#         # print("*", end="")
-#
-#         if i == 0 and j ==
-#     print()
 
-for i in range(1, n+1): # 1 2 3 4 5
-    print(" " * (n-i) + "*" * i)
+for i in range(1, n+1):
+    print(" "*(n-i) + '*' * i)
