80754758f74b5687258d900193d932d7f53bc9b7
ChanYoungHong <cksdud9883@naver.com>
2023-06-10 17:09:27 +0900

    이번 주 푼 문제 복습하기

--- BAEKJOON/구현/BOJ_1764_듣보잡.py
+++ BAEKJOON/구현/BOJ_1764_듣보잡.py
@@ -2,42 +2,26 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
 
-# a1 = []
-# for i in range(n):
-#     a1.append(input().rstrip())
-#
-# a2 = []
-# for i in range(m):
-#     a2.append(input().rstrip())
-#
-# a1.sort()
-# a2.sort()
-#
-# def bs(start, end, target):
-#
-#
-#     while start <= end:
-#         mid = (start + end) // 2
-#
-#         if a1[mid] == target:
-#             return True
-#         elif a1[mid] < target:
-#             start = mid + 1
-#         elif a1[mid] > target:
-#             end = mid - 1
-#
-#     return False
-#
-# res = []
-# for i in a2:
-#     if bs(0, len(a1)-1, i):
-#         res.append(i)
-#
-# res.sort()
-# print(len(res))
-# for z in res:
-#     print(z)
+n,m = map(int, input().split())
+
+arr1 = set()
+for i in range(n):
+
+    arr1.add(input().rstrip())
+
+arr2 = set()
+
+for i in range(m):
+    arr2.add(input().rstrip())
+
+ans = set(arr1 & arr2)
 
 
+# print(arr1)
+# print(arr2)
+# print(ans)
+
+print(len(ans))
+for i in sorted(ans):
+    print(i)
--- BAEKJOON/정렬/BOJ_10816_숫자카드2.py
+++ BAEKJOON/정렬/BOJ_10816_숫자카드2.py
@@ -1,35 +1,42 @@
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
 n = int(input())
-arr1 = list(sorted(map(int, input().split())))
+# arr1 = list(sorted(map(int, input().split())))
+arr1 = list(map(int, input().split()))
 m = int(input())
 arr2 = list(map(int, input().split()))
 
+# def bs(arr, start, end, target):
+#     if start > end:
+#         return 0
+#
+#     mid = (start + end) // 2
+#
+#     if arr[mid] == target:
+#         return cnt.get(target)
+#     elif arr[mid] > target:
+#         return bs(arr, start, mid - 1, target)
+#         # end = mid - 1
+#     elif arr[mid] < target:
+#         return bs(arr, mid + 1, end, target)
+#         # start = mid + 1
+#
+#
+# cnt = {}
+# for i in arr1:
+#     if i in cnt:
+#         cnt[i] += 1
+#     else:
+#         cnt[i] = 1
+#
+# for i in arr2:
+#     print(bs(arr1, 0, len(arr1) - 1, i), end=' ')
 
-def bs(arr, start, end, target):
-    if start > end:
-        return 0
 
-    mid = (start + end) // 2
-
-    if arr[mid] == target:
-        return cnt.get(target)
-    elif arr[mid] > target:
-        return bs(arr, start, mid - 1, target)
-        # end = mid - 1
-    elif arr[mid] < target:
-        return bs(arr, mid + 1, end, target)
-        # start = mid + 1
-
-
-cnt = {}
-for i in arr1:
-    if i in cnt:
-        cnt[i] += 1
-    else:
-        cnt[i] = 1
+z = Counter(arr1)
 
 for i in arr2:
-    print(bs(arr1, 0, len(arr1) - 1, i), end=' ')
+    print(z[i], end=' ')
