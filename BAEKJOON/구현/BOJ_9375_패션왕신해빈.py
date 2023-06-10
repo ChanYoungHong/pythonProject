4d59adfb41391e702638cea7931d948040140bae
ChanYoungHong <cksdud9883@naver.com>
2023-06-10 15:17:28 +0900

    이번 주 공부한 내용 다시 복습하기

--- BAEKJOON/구현/BOJ_9375_패션왕신해빈.py
+++ BAEKJOON/구현/BOJ_9375_패션왕신해빈.py
@@ -1,20 +1,22 @@
 import sys
 from collections import Counter
 
+input = sys.stdin.readline
+
 t = int(input())
 
 for i in range(t):
 
     n = int(input())
-    s = []
+    arr = []
     for j in range(n):
-        a, b = input().split()
-        s.append(b)
+        a,b = input().split()
+        arr.append(b)
 
     num = 1
-    result = Counter(s)
-    print('result : ', result)
-    for key in result:
-        num *= result[key] + 1
-        print('result[key] : ', result[key])
+    res = Counter(arr)
+
+    for key in res:
+        num *= res[key] + 1
+
     print(num - 1)
