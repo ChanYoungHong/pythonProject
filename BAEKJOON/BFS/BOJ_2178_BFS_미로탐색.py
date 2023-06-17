76fdc37b7363b4a6c192b0c758a89e7c5131b56a
ChanYoungHong <cksdud9883@naver.com>
2023-06-17 20:28:46 +0900

    알듯 말듯 bfs 80% 알겠음 뭔가 이해가 완벽하게 안 된 상태인 듯

--- BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
+++ BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
@@ -3,34 +3,32 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
+n,m = map(int, input().split())
+board = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
 
 
-def bfs(x, y):
+def bfs(x,y):
 
     q = deque()
-    q.append((x, y))
+    q.append((x,y))
 
     while q:
 
-        x, y = q.popleft()
+        x,y = q.popleft()
 
         for i in range(4):
+
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = graph[x][y] + 1
-                    q.append((nx, ny))
+                if board[nx][ny] == 1:
+                    board[nx][ny] = board[x][y] + 1
+                    q.append((nx,ny))
 
-    return graph[-1][-1]
+    return board[-1][-1]
 
 print(bfs(0,0))
-
-
-
--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -1,13 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+'''
+1. 1 < n < 1000 사이에 있으니깐 
+2. 이중 for문으로 풀 수 있을 듯
+
+'''
 
 n = int(input())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-dp = [1] * n
+dp = [1] *n
 
 for i in range(n):
     for j in range(i):
 
-        if nums[i] > nums[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
+# print(dp)
 print(max(dp))
+
+
