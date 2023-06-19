7242579ed31f4781c017c294cb87833fb1991794
ChanYoungHong <cksdud9883@naver.com>
2023-06-19 21:11:11 +0900

    2일 전에 못 푼 bfs 복습완료

--- BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
+++ BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
@@ -3,17 +3,17 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
+n, m = map(int, input().split())
+
 board = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 
-def bfs(x,y):
-
+def bfs(x, y):
     q = deque()
-    q.append((x,y))
+    q.append((x, y))
 
     while q:
 
