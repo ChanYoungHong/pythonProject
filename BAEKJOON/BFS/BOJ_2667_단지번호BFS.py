361fd0e4422258088c3fed85ad74a908b7ed4b5b
ChanYoungHong <cksdud9883@naver.com>
2023-06-19 21:02:04 +0900

    저번 주 못 풀었던 문제 다시

--- BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
+++ BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
@@ -1,14 +1,16 @@
+import sys
 from collections import deque
 
+input = sys.stdin.readline
 
 
 n = int(input())
-board = [list(map(int, input())) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
+board = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
+cnt = 0
 
 def bfs(x,y):
 
@@ -23,29 +25,29 @@ def bfs(x,y):
         x,y = q.popleft()
 
         for i in range(4):
-
             nx = x + dx[i]
             ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < n:
                 if board[nx][ny] == 1:
                     board[nx][ny] = 0
-                    q.append((nx,ny))
                     cnt += 1
+                    q.append((nx,ny))
 
     return cnt
 
 res = []
-
 for i in range(n):
     for j in range(n):
 
-        # 새로운 영역의 1을 만났을 대 res에 넣음
         if board[i][j] == 1:
             board[i][j] = 0
             res.append(bfs(i,j))
 
 
+
+res.sort()
 print(len(res))
-for i in sorted(res):
+for i in res:
     print(i)
+
