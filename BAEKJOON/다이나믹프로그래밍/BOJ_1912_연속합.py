1f2105b1452e6e1ed88c777c35a12f1b33dbaf03
ChanYoungHong <cksdud9883@naver.com>
2023-06-22 16:47:12 +0900

    2달 전에 풀었던 문제 다시 복습, 근데 어떻게 푸는 모르겠음

--- BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
@@ -1,26 +1,30 @@
-
 import sys
 
 input = sys.stdin.readline
 
+'''
+
+풀어 나가는 방향성은 ?
+-> 더하는 기준의 길이를 1씩 높여 나가면서 모든 경우의 수를 다 더해서 
+
+새로운 배열을 거기 만들어서 넣은 후에 max()로 꺼내기 ?
+약간 슬라이딩 윈도우 같은 느낌으로 ?
+
+이전까지 모든 숫자의 합 중 최대값을 구하여라.
+'''
+
 n = int(input())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-dp = [0] * (n+1)
-# summ = [sum(nums[:2])]
-#
-# for i in range(n-2):
-#     summ.append(summ[i] - nums[i] + nums[i+2])
-#
-# print(max(summ))
-
-summ = [nums[0]]
+dp = [arr[0]]
 
 for i in range(n-1):
-    summ.append(max(summ[i]+nums[i+1], nums[i+1]))
+    dp.append(max(dp[i] + arr[i+1], arr[i+1]))
 
-print(max(summ))
-print(summ)
+print(dp)
+
+
+
 
 
 
