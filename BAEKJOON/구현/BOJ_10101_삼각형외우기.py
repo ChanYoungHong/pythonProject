02c96c8e8af4a8dc383ac787a3fd50853716c900
ChanYoungHong <cksdud9883@naver.com>
2023-06-26 15:25:38 +0900

    문제 풀었던 것 복습

--- BAEKJOON/구현/BOJ_10101_삼각형외우기.py
+++ BAEKJOON/구현/BOJ_10101_삼각형외우기.py
@@ -2,15 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-arr = [int(input()) for _ in range(3)]
+a = int(input())
+b = int(input())
+c = int(input())
 
 
-if sum(arr) == 180:
-    if arr[0] == arr[1] == arr[2]:
-        print('Equilateral')
-    elif sum == 180 and arr[0] == arr[1] or arr[1] == arr[2] or arr[0] == arr[2]:
+if a == b == c:
+    print('Equilateral')
+elif (a+b+c) == 180:
+    if a == b or b == c or a == c:
         print('Isosceles')
-    elif sum == 180 and arr[0] != arr[1] or arr[1] != arr[2] or arr[0] != arr[2]:
+    elif a != b or b != c or a != c:
         print('Scalene')
-else:
+elif (a+b+c) != 180:
     print('Error')
