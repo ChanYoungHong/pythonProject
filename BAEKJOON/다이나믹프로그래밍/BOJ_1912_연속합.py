c7f0d29582511f786db10fdeb69fbcde51871246
ChanYoungHong <cksdud9883@naver.com>
2023-06-28 23:39:40 +0900

    5일 전 문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
@@ -14,16 +14,15 @@ input = sys.stdin.readline
 '''
 
 n = int(input())
+
 arr = list(map(int, input().split()))
 
 dp = [arr[0]]
 
 for i in range(n-1):
-    dp.append(max(dp[i] + arr[i+1], arr[i+1]))
+    dp.append(max(dp[i]+arr[i+1], arr[i+1]))
 
-print(dp)
-
-
+print(max(dp))
 
 
 
