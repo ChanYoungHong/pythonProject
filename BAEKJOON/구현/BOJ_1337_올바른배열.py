415f9ec463a5fdf4c0b171d133124a961c2333b8
ChanYoungHong <cksdud9883@naver.com>
2023-06-28 23:35:13 +0900

    5일 전 문제 복습

--- BAEKJOON/구현/BOJ_1337_올바른배열.py
+++ BAEKJOON/구현/BOJ_1337_올바른배열.py
@@ -10,19 +10,16 @@ input = sys.stdin.readline
 '''
 
 n = int(input())
-
-arr = []
-temp = []
-
 arr = [int(input()) for _ in range(n)]
+res = []
+
 
 for i in range(n):
     cnt = 0
-
     for j in range(arr[i], arr[i]+5):
-
         if j not in arr:
             cnt += 1
-    temp.append(cnt)
 
-print(min(temp))
+    res.append(cnt)
+
+print(min(res))
