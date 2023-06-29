e938f6b7f34ab2b4fb4017850a5f9f7d6ad51f4d
ChanYoungHong <cksdud9883@naver.com>
2023-06-29 17:40:47 +0900

    어제 못 풀었던 문제 다시 복습

--- BAEKJOON/구현/BOJ_1316_그룹단어체커.py
+++ BAEKJOON/구현/BOJ_1316_그룹단어체커.py
@@ -4,21 +4,24 @@ input = sys.stdin.readline
 
 n = int(input())
 
-answer = 0
+# arr = [input().rstrip() for _ in range(n)]
+
+
+ans = 0
 for i in range(n):
 
-    word = input()
-
+    word = input().rstrip()
     cnt = 0
+
     for j in range(len(word) - 1):
 
         if word[j] != word[j+1]:
-            new_word = word[j+1:]
+           new_word = word[j+1:]
 
-            if new_word.count(word[j]) > 0:
-                cnt += 1
+           if new_word.count(word[j]) > 0:
+               cnt += 1
 
     if cnt == 0:
-        answer += 1
+        ans += 1
 
-print(answer)
+print(ans)
