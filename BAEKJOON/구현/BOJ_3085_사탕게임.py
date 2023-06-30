62fd6f696e88e9fef900579f5c45dc66dbf5b7a8
ChanYoungHong <cksdud9883@naver.com>
2023-06-30 17:49:41 +0900

    2달 전 문제 복습... 새롭다;;

--- BAEKJOON/구현/BOJ_3085_사탕게임.py
+++ BAEKJOON/구현/BOJ_3085_사탕게임.py
@@ -9,60 +9,69 @@ input = sys.stdin.readline
 
 무슨 사탕을 기준으로 최대를 구하나 ?? 
 
+- 부르프 포스를 사용
+- 가장 긴 연속 부분 행 or 열을 고른 다음에 사탕을 모두 먹는다.
+- 사탕이 채워진 상태에서 상근이가 먹을 수 있는 사탕의 최대 개수는 ?
+
+푸는 방향 인접한 모든 사탕을 다 뒤짚어보고 
+먹을 수 있는 최대 개수를 구하는 방향으로 풀기
 '''
 
 
-
-
-def count_candy():
-    row_cnt, col_cnt, row_max, col_max = 1, 1, -1e9, -1e9
-
-    # 행을 check 한다.
-    for i in range(n):
-        for j in range(n - 1):
-
-            if board[i][j] == board[i][j + 1]:
-                row_cnt += 1
-            else:
-                row_cnt = 1
-            row_max = max(row_cnt, row_max)
-        row_cnt = 1
-
-    # 열을 check 한다.
-    for j in range(n):
-        for i in range(n - 1):
-
-            if board[i][j] == board[i + 1][j]:
-                col_cnt += 1
-            else:
-                col_cnt = 1
-            col_max = max(col_cnt, col_max)
-        col_cnt = 1
-    answer = max(row_max, col_max)
-
-    return answer
-
 n = int(input())
 board = [list(input().rstrip()) for _ in range(n)]
+maxCnt = 0
 
-steps = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-ans = 0
+def count_candy():
+    global maxCnt
+
+    # 행 검사
+    for i in range(n):
+        cnt = 1
+        for j in range(1, n):
+            if board[i][j] == board[i][j-1]:
+                cnt += 1
+                maxCnt = max(cnt, maxCnt)
+            else:
+                cnt = 1
+
+        # 열 검사
+        cnt = 1
+        for j in range(1, n):
+            if board[j][i] == board[j-1][i]:
+                cnt += 1
+                maxCnt = max(cnt, maxCnt)
+            else:
+                cnt = 1
+
+
 
 for i in range(n):
     for j in range(n):
 
-        # 0 1 2 3
-        for k in range(4):
-            nx = i + steps[k][0]
-            ny = j + steps[k][1]
+        # 오른쪽과 바꿈
+        if j + 1 < n:
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
+            count_candy()
+            board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
+        # 아래쪽과 바꿈
+        if i + 1 < n:
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
+            count_candy()
+            board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
 
-            if board[i][j] != board[nx][ny]:
-                board[nx][ny], board[i][j] = board[i][j], board[nx][ny]
-                ans = max(ans, count_candy())
-                # 다시 재자리로 돌려준다 왜 ?
-                board[i][j], board[nx][ny] = board[nx][ny], board[i][j]
+print(maxCnt)
 
-print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
