836d2e88afd9f4de56b46b22f09ddf74b0702e52
ChanYoungHong <cksdud9883@naver.com>
2023-06-30 16:48:57 +0900

    6/29일에 풀었던 것 복습

--- BAEKJOON/구현/BOJ_5525_IOIOI.py
+++ BAEKJOON/구현/BOJ_5525_IOIOI.py
@@ -4,43 +4,22 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-S = input().rstrip()
+s = input()
 
-check = 'IOI' * n
+ans, i, cnt = 0,0,0
 
-answer, i, count = 0,0,0
 
 while i < (m-1):
 
-    if S[i:i+3] == 'IOI':
+    if s[i:i+3] == 'IOI':
         i += 2
-        count += 1
+        cnt += 1
 
-        if count == n:
-            answer += 1
-            count -= 1
+        if n == cnt:
+            cnt -= 1
+            ans += 1
     else:
         i += 1
-        count = 0
+        cnt = 0
 
-print(answer)
-
-
-# print(S.count(check)+1)
-
-# cnt = 0
-# for i in range(len(S) - len(check)):
-#     for j in range(len(S)):
-#
-#         if S[i] +  == check:
-#             cnt += 1
-#         print(S[i + 2])
-#
-# print(cnt)
-
-# S = S.replace(check, 'I')
-#
-# S = S.replace(check, 'I')
-
-# print(S)
-# print(len(S) - 1)
+print(ans)
