4975e4cf35ae9fe6d9b674da99be616970dc9857
ChanYoungHong <cksdud9883@naver.com>
2023-07-04 17:08:20 +0900

    이진 탐색문제 복습

--- BAEKJOON/BOJ_10815.py
+++ BAEKJOON/BOJ_10815.py
@@ -1,28 +1,44 @@
-n = int(input())  # 5개 상근이 가진 카드
-array1 = list(map(int, input().split()))
+import sys
 
-m = int(input())  # 8개 상근이 가졌는지 확인해야 할 카드
-array2 = list(map(int, input().split()))
+input = sys.stdin.readline
 
-array1.sort()
+'''
+1. 아이디어
+
+2. 시간복잡도 ?
+    N이 500,000까지라 이진 탐색을 사용해야 함
+3.
+
+'''
+
+# 상근이가 가지고 있는 숫자 카드
+n = int(input())
+arr1 = list(map(int, input().split()))
 
 
-def binary_search(array1, target, start, end):
+# 구분해야 할 정수 카드
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+arr1.sort()
+
+def bs(arr, start, end, target):
 
     while start <= end:
+
         mid = (start + end) // 2
 
-        if array1[mid] == target:
-            return mid
-        elif array1[mid] > target:
-            end = mid - 1
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] < target:
+            start = mid + 1
         else:
-            start = start + 1
+            end = mid - 1
 
-    return None
 
 for i in range(m):
-    if binary_search(array1,array2[i],0,n-1) is not None:
+
+    if bs(arr1, 0, len(arr1) - 1, arr2[i]) is not None:
         print(1, end=' ')
     else:
         print(0, end=' ')
