debf2920ecc3299279d53324a9470e46cbb6fba4
ChanYoungHong <cksdud9883@naver.com>
2023-07-04 18:44:09 +0900

    구현문제 복습

--- BAEKJOON/구현/BOJ_1475_방번호.py
+++ BAEKJOON/구현/BOJ_1475_방번호.py
@@ -1,13 +1,16 @@
 import sys
 
 input = sys.stdin.readline
-
-number = input().rstrip()
+'''
+1. 아이디어 
+2. 시간복잡도 - 
+3. 
+'''
+n = input().rstrip()
 
 sett = [0] * 9
 
-
-for i in number:
+for i in n:
 
     idx = int(i)
 
@@ -15,6 +18,7 @@ for i in number:
         idx = 6
     sett[idx] += 1
 
-
 sett[6] = (sett[6] + 1) // 2
 print(max(sett))
+
+
