81affa26f9dbb4b4c59f35b2e8fa40b4e3ae7d79
ChanYoungHong <cksdud9883@naver.com>
2023-07-05 16:01:05 +0900

    어제 문제 복습

---
+++ BAEKJOON/이진탐색/BOJ_7795_먹을것인가먹힐것인가.py
@@ -0,0 +1,51 @@
+import sys
+
+input = sys.stdin.readline
+
+'''
+1. 아이디어 - 20000 사이라서 nlongn 써야해서 이진탐색으로 접근
+2. 시간복잡도 - 20000 사이라서 nlongn 써야해서 이진탐색으로 접근
+3.
+
+이진탐색 알고리즘 특징 - 
+이진 탐색이란 데이터가 정렬돼 있는 배열에서 특정한 값을 찾아내는 알고리즘이다.
+
+'''
+
+t = int(input())
+
+aarr = []
+barr = []
+
+def bs(arr, start, end, target):
+
+    res = -1
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        if arr[mid] < target:
+            res = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+    return res
+
+
+for _ in range(t):
+    a,b = map(int, input().rstrip().split())
+
+    aarr = list(map(int, input().split()))
+    barr = list(map(int, input().split()))
+
+    barr.sort()
+    cnt = 0
+
+    for i in aarr:
+        cnt += bs(barr, 0, len(barr) - 1, i) + 1
+
+    print(cnt)
+
+
+
+
