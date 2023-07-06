a38e5863ef34dbd4d320e92af5739a27fb77b630
ChanYoungHong <cksdud9883@naver.com>
2023-07-06 15:35:56 +0900

    3일전 문제 복습

--- BAEKJOON/구현/BOJ_1316_그룹단어체커.py
+++ BAEKJOON/구현/BOJ_1316_그룹단어체커.py
@@ -2,26 +2,32 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+'''
+1. 아이디어 - 2중 for문 돌려서 -> new_word.count()를 사용하기 -> 
+0보다 큰 거 개수 따로 새기 cnt, ans로 나눠서
+cnt == 0일 때 ans += 1해주기
 
-# arr = [input().rstrip() for _ in range(n)]
+2. 시간복잡도 - 적당한 3중 루프 다 돌아감
+3. 문법 - 
+'''
 
+t = int(input())
 
 ans = 0
-for i in range(n):
+for _ in range(t):
 
-    word = input().rstrip()
+    word = input()
     cnt = 0
+    for i in range(len(word) - 1):
 
-    for j in range(len(word) - 1):
+        if word[i] != word[i+1]:
+            new_word = word[i+1:]
 
-        if word[j] != word[j+1]:
-           new_word = word[j+1:]
-
-           if new_word.count(word[j]) > 0:
-               cnt += 1
+            if new_word.count(word[i]) > 0:
+                cnt += 1
 
     if cnt == 0:
         ans += 1
 
 print(ans)
+
