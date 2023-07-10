7bdb74ea9a92859be7002d01d5f50fdaadee56cc
ChanYoungHong <cksdud9883@naver.com>
2023-07-10 18:54:15 +0900

    이진탐색 복습

--- BAEKJOON/이진탐색/BOJ_7795_먹을것인가먹힐것인가.py
+++ BAEKJOON/이진탐색/BOJ_7795_먹을것인가먹힐것인가.py
@@ -10,41 +10,44 @@ input = sys.stdin.readline
 이진탐색 알고리즘 특징 - 
 이진 탐색이란 데이터가 정렬돼 있는 배열에서 특정한 값을 찾아내는 알고리즘이다.
 
+A의 크기가 B보다 큰 쌍이 몇 개나 있는지 구하는 프로그램을 만드시오
+
 '''
 
 t = int(input())
-
-aarr = []
-barr = []
 
 def bs(arr, start, end, target):
 
     res = -1
     while start <= end:
-
         mid = (start + end) // 2
 
+        # arr2은 arr임  target : arr1 이고
         if arr[mid] < target:
             res = mid
-            print('res : ', res)
             start = mid + 1
         else:
             end = mid - 1
     return res
 
+# 0 1 2
+# 1 3 6
+# res = 1
+#
 
 for _ in range(t):
     a,b = map(int, input().rstrip().split())
 
-    aarr = list(map(int, input().split()))
-    barr = list(map(int, input().split()))
+    arr1 = list(map(int, input().split()))
 
-    barr.sort()
+    # 정렬되어 있는 알고리즘에서 특정한 값을 찾을 때
+    arr2 = list(map(int, input().split()))
+
+    arr2.sort()
     cnt = 0
 
-    for i in aarr:
-        cnt += bs(barr, 0, len(barr) - 1, i) + 1
-        print('bs(barr, 0, len(barr) - 1, i) :', bs(barr, 0, len(barr) - 1, i))
+    for i in arr1:
+        cnt += bs(arr2, 0, len(arr2) - 1, i) + 1
 
     print(cnt)
 
