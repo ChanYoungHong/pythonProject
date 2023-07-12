5e304bf7df26aba651ede91c13e5fe37c63ecd9a
ChanYoungHong <cksdud9883@naver.com>
2023-07-12 17:53:48 +0900

    투 포인터 연습 문제

--- BAEKJOON/투포인터/BOJ_2003_수들의합2.py
+++ BAEKJOON/투포인터/BOJ_2003_수들의합2.py
@@ -3,22 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().rstrip().split()))
 
-cnt = 0
-inter_num = 0
 end = 0
+inter_num = 0
+cnt = 0
 
 for i in range(n):
 
     while inter_num < m and end < n:
-
-        inter_num += nums[end]
+        inter_num += arr[end]
         end += 1
 
     if inter_num == m:
         cnt += 1
 
-    inter_num -= nums[i]
+    inter_num -= arr[i]
 
 print(cnt)
