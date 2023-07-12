3e7c19054bd1327f3ae7b6938b4933178eb5c846
ChanYoungHong <cksdud9883@naver.com>
2023-07-12 15:29:14 +0900

    구현문제 복습

--- BAEKJOON/구현/BOJ_2941_크로아티아알파벳.py
+++ BAEKJOON/구현/BOJ_2941_크로아티아알파벳.py
@@ -1,11 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
 alph = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 word = input()
 
-
 for i in alph:
-
     word = word.replace(i, '*')
 
-print(len(word))
-
+print(len(word) - 1)
