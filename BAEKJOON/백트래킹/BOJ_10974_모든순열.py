906d4c38cf3646e1756e66fb3af2d4476d63b2d5
ChanYoungHong <cksdud9883@naver.com>
2023-07-13 17:25:35 +0900

    dfs문제 복습 완료

--- BAEKJOON/백트래킹/BOJ_10974_모든순열.py
+++ BAEKJOON/백트래킹/BOJ_10974_모든순열.py
@@ -2,21 +2,19 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+n = int(input().rstrip())
 
 arr = []
-
 def dfs():
 
     if len(arr) == n:
         print(*arr)
 
-    for i in range(1, 1+n):
 
+    for i in range(1, n+1):
         if i not in arr:
             arr.append(i)
             dfs()
             arr.pop()
 
 dfs()
-
