2c9890b64a7fdc11ca72ff360cf412bafc139d26
ChanYoungHong <cksdud9883@naver.com>
2023-07-13 19:58:49 +0900

    3달 전 문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_11057_오르막수.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11057_오르막수.py
@@ -1,15 +1,17 @@
 import sys
 
 input = sys.stdin.readline
+'''
+1. 알고리즘 - 
+2. 시간복잡도 - 1000 사이라 dp 사용하면 충분
+3. 배열? 뭐 키워드 - 
 
+'''
 n = int(input())
+dp = [1] * 10
 
-dp = [0] * 10
-dp[0] = 1
-
-for i in range(1, n+1):
+for i in range(1, n):
     for j in range(1, 10):
-
         dp[j] += dp[j-1]
 
-print(sum(dp) % 10007)
+print(sum(dp)%10007)
