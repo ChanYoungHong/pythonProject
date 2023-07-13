1dbe38278c4394107767d6035c449bd259439731
ChanYoungHong <cksdud9883@naver.com>
2023-07-13 18:07:21 +0900

    투 포인터 복습완료2

--- BAEKJOON/투포인터/BOJ_2559_수열.py
+++ BAEKJOON/투포인터/BOJ_2559_수열.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-temp = [sum(nums[:m])]
+dp = [sum(arr[:m])]
 
-for j in range(n - m):  # 0 1 2 3 4 5 6 7 8 9
-    temp.append(temp[j] - nums[j] + nums[j + m])
+for i in range(n - m):
+    dp.append(dp[i] - arr[i] + arr[m+i])
 
-print(max(temp))
+print(max(dp))
