08c751ab58a7391742726a0ac16b798e512d6eac
ChanYoungHong <cksdud9883@naver.com>
2023-07-15 15:33:47 +0900

    브루트포스 알고리즘 복습

--- BAEKJOON/문자열/BOJ_1120_문자열.py
+++ BAEKJOON/문자열/BOJ_1120_문자열.py
@@ -5,24 +5,26 @@ input = sys.stdin.readline
 
 '''
 
-1. 알고리즘 - 
+1. 알고리즘 - A의 길이는 B의 길이보다 작거나 같다, A <= B
 2. 시간복잡도 - 따로 알고리즘 필요 없을 듯 웬만하면 다 됨
 3. 어떤 방식? - 
 '''
 
-word1, word2 = input().split()
-res = []
+word1, word2 = input().rstrip().split()
+ans = []
 
 for i in range(len(word2) - len(word1) + 1):
-    cnt = 0
 
+    cnt = 0
+    # 0 1 2 3 4
     for j in range(len(word1)):
+
         if word1[j] != word2[i+j]:
             cnt += 1
+    ans.append(cnt)
 
-    res.append(cnt)
+print(min(ans))
 
-# print(res)
-print(min(res))
+
 
 
