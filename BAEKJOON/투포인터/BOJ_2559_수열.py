9deb7976cda23ece84626f1830c38f593e082ef3
ChanYoungHong <cksdud9883@naver.com>
2023-07-17 14:57:29 +0900

    dp문제 복습

--- BAEKJOON/투포인터/BOJ_2559_수열.py
+++ BAEKJOON/투포인터/BOJ_2559_수열.py
@@ -2,12 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+'''
+1. 알고리즘 - 
+2. 시간복잡도 - 
+3. 배열 -  
+'''
+
+n,m = map(int, input().split())
 arr = list(map(int, input().split()))
 
 dp = [sum(arr[:m])]
 
-for i in range(n - m):
+for i in range(n-m):
     dp.append(dp[i] - arr[i] + arr[m+i])
 
 print(max(dp))
