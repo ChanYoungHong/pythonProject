13ed7b56174cdb5033b7231cc181e0696f750d61
ChanYoungHong <cksdud9883@naver.com>
2023-07-17 15:17:50 +0900

    dp문제 복습2

--- BAEKJOON/다이나믹프로그래밍/BOJ_11057_오르막수.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11057_오르막수.py
@@ -12,8 +12,9 @@ n = int(input())
 
 dp = [1] * 10
 
-for i in range(1 , n):
+for i in range(1, n):
     for j in range(1, 10):
+
         dp[j] += dp[j-1]
 
-print(sum(dp) % 100007)
+print(sum(dp)%10007)
