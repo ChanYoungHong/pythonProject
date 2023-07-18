80520eb9ee917b6ebf1f1ff83b50f596acad1094
ChanYoungHong <cksdud9883@naver.com>
2023-07-18 16:55:12 +0900

    dfs 코드 분석 및 복습

--- BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
+++ BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
@@ -3,51 +3,42 @@ from collections import deque
 
 input = sys.stdin.readline
 
-
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 n = int(input())
 board = [list(map(int, input().rstrip())) for i in range(n)]
-
-cnt = 0
 res = []
 
-def bfs(x,y):
+cnt = 0
+
+def dfs(x,y):
 
     global cnt
-    cnt = 1
 
-    q = deque()
-    q.append((x,y))
+    board[x][y] = 0
+    cnt += 1
 
-    while q:
 
-        x,y = q.popleft()
+    for i in range(4):
+        nx = dx[i] + x
+        ny = dy[i] + y
 
-        for i in range(4):
+        if 0 <= nx < n and 0 <= ny < n:
+            if board[nx][ny] == 1:
+                dfs(nx,ny)
 
-            nx = x + dx[i]
-            ny = y + dy[i]
+res = []
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if board[nx][ny] == 1:
-                    board[nx][ny] = 0
-                    cnt += 1
-                    q.append((nx,ny))
-
-    return cnt
-
-house = 0
 for i in range(n):
     for j in range(n):
 
         if board[i][j] == 1:
-            board[i][j] = 0
-            res.append(bfs(i,j))
+            dfs(i,j)
+            res.append(cnt)
+            cnt = 0
 
-
-print(len(res))
-for i in sorted(res):
+print(res)
+res.sort()
+for i in res:
     print(i)
-
