a8e2d94609f000eef0bae80d9b92ca064d3df914
ChanYoungHong <cksdud9883@naver.com>
2023-07-19 15:40:59 +0900

    dfs 문제복습2

--- BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
+++ BAEKJOON/DFS/BOJ_2667_단지번호붙이기.py
@@ -7,9 +7,7 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 n = int(input())
-board = [list(map(int, input().rstrip())) for i in range(n)]
-res = []
-
+board = [list(map(int, input().rstrip())) for _ in range(n)]
 cnt = 0
 
 def dfs(x,y):
@@ -19,7 +17,6 @@ def dfs(x,y):
     board[x][y] = 0
     cnt += 1
 
-
     for i in range(4):
         nx = dx[i] + x
         ny = dy[i] + y
@@ -28,17 +25,19 @@ def dfs(x,y):
             if board[nx][ny] == 1:
                 dfs(nx,ny)
 
-res = []
+    return cnt
 
+
+res = []
 for i in range(n):
     for j in range(n):
 
         if board[i][j] == 1:
-            dfs(i,j)
-            res.append(cnt)
+            res.append(dfs(i,j))
             cnt = 0
 
-print(res)
+print(len(res))
 res.sort()
+
 for i in res:
     print(i)
