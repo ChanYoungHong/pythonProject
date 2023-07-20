6ec4f2ba241e402e9dedc215789f5883b88bf979
ChanYoungHong <cksdud9883@naver.com>
2023-07-20 12:04:32 +0900

    이진탐색 풀이완료

--- BAEKJOON/이진탐색/BOJ_1920_수찾기.py
+++ BAEKJOON/이진탐색/BOJ_1920_수찾기.py
@@ -9,28 +9,30 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-array1 = list(map(int, input().split()))
+arr1 = list(map(int, input().split()))
 m = int(input())
-array2 = list(map(int, input().split()))
-
-array1 = list(set(array1))
-array1.sort()
+arr2 = list(map(int, input().split()))
 
 
-def binary_search(array, target):
-    start = 0
-    end = len(array) - 1
+arr1.sort()
+
+def bs(arr, start, end, target):
 
     while start <= end:
+
         mid = (start + end) // 2
-        if array[mid] == target:
+
+        if arr[mid] == target:
             return 1
-        elif array[mid] < target:
+        elif arr[mid] < target:
             start = mid + 1
         else:
             end = mid - 1
-    return 0
 
 
-for i in array2:
-    print(binary_search(array1, i))
+for i in arr2:
+    if bs(arr1, 0, len(arr1) - 1, i) is not None:
+        print(1)
+    else:
+        print(0)
+
