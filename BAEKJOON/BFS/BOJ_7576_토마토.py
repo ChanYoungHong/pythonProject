f44525e707483f0c173fe139ecfda0bb36625c32
ChanYoungHong <cksdud9883@naver.com>
2023-07-24 17:32:47 +0900

    토마토 - bfs문제 이해 및 복습완료

--- BAEKJOON/BFS/BOJ_7576_토마토.py
+++ BAEKJOON/BFS/BOJ_7576_토마토.py
@@ -6,35 +6,44 @@ input = sys.stdin.readline
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-q = deque([])
+q = deque()
+
 for i in range(n):
     for j in range(m):
+
         if graph[i][j] == 1:
-            q.append([i, j])
+            q.append((i,j))
+
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def bfs():
+
+    while q:
+
+        x,y = q.popleft()
+
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx,ny))
+
+
+bfs()
 
 res = 0
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-
-def bfs():
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append([nx, ny])
-
-bfs()
 for i in graph:
     for j in i:
         if j == 0:
             print(-1)
-            exit()
+            exit(0)
     res = max(res, max(i))
-print(res - 1)
+
+print(res)
+
