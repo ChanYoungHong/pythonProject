2ba2d1e4a0c4b2d625444f52d7f5bf28059fbb99
ChanYoungHong <cksdud9883@naver.com>
2023-07-24 14:55:54 +0900

    안전영역 - dfs문제 이해 및 복습완료

--- BAEKJOON/DFS/BOJ_2468_안전영역.py
+++ BAEKJOON/DFS/BOJ_2468_안전영역.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 
 input = sys.stdin.readline
 
@@ -16,16 +16,18 @@ board = [list(map(int, input().rstrip().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+
 def dfs(x,y,h):
 
     for i in range(4):
+
         nx = dx[i] + x
         ny = dy[i] + y
 
         if 0 <= nx < n and 0 <= ny < n:
-            if visited[nx][ny] == False and board[nx][ny] > h:
+            if board[nx][ny] > k and not visited[nx][ny]:
                 visited[nx][ny] = True
-                dfs(nx,ny, h)
+                dfs(nx,ny,h)
 
 
 res = []
@@ -37,16 +39,12 @@ for k in range(max(map(max, board))):
     for i in range(n):
         for j in range(n):
 
-            if board[i][j] > k and visited[i][j] == False:
+            if board[i][j] > k and not visited[i][j]:
                 visited[i][j] = True
                 cnt += 1
                 dfs(i,j,k)
-
     res.append(cnt)
-    answer = max(res)
-
-print(answer)
+    ans = max(res)
 
 
-
-
+print(ans)
