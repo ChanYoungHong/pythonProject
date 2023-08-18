5e7be1a3cb5715c29e4c1a154b55ee983f3b1970
ChanYoungHong <cksdud9883@naver.com>
2023-08-18 14:18:41 +0900

    백트래킹 문제 복습

--- BAEKJOON/DFS/BOJ_1189_컴백홈.py
+++ BAEKJOON/DFS/BOJ_1189_컴백홈.py
@@ -2,39 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
-'''
-1. 갔던 곳은 안 간다고 함
-2. 
-
-'''
-
-r, c, k = map(int, input().split())
-
-board = [list(input().rstrip()) for _ in range(r)]
-
 dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+dy = [0, 0, 1, -1]
 
-# 겹치지 않는 방향?을 저장해야 함
-answer = 0
 
-def dfs(x,y,dep):
-
+def dfs(x, y, depth):
     global answer
 
-    if dep == k and x == 0 and y == c - 1:
+    if depth == k and x == 0 and y == c - 1:
         answer += 1
+    else:
+
+        board[x][y] = 'T'
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < r and 0 <= ny < c and board[nx][ny] == '.':
+                board[nx][ny] = 'T'
+                dfs(nx, ny, depth + 1)
+                board[nx][ny] = '.'
 
 
-    for i in range(4):
-        nx = dx[i] + x
-        ny = dy[i] + y
-
-        if 0 <= nx < r and 0 <= ny < c and board[nx][ny] == '.':
-            board[nx][ny] = 'T'
-            dfs(nx,ny, dep + 1)
-            board[nx][ny] = '.'
-
-
-dfs(r-1, 0, 1)
+r, c, k = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(r)]
+answer = 0
+dfs(r-1,0,1)
 print(answer)
