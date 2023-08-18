d678bfffb41102fc33182563f303f2b4c339a101
ChanYoungHong <cksdud9883@naver.com>
2023-08-18 15:46:54 +0900

    정렬 문제복습

---
+++ BAEKJOON/정렬/BOJ_2910_빈도정렬.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dic = dict()
+
+for i in arr:
+    if i not in dic:
+        dic[i] = 0
+    dic[i] += 1
+
+dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+
+for a,b in dic:
+    for j in range(b):
+        print(str(a), end=' ')
