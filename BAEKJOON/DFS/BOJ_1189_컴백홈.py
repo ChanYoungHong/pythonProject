95a14b5c86cf1972787f1c824f809d227ac05360
ChanYoungHong <cksdud9883@naver.com>
2023-08-19 13:03:18 +0900

    복습2

--- BAEKJOON/DFS/BOJ_1189_컴백홈.py
+++ BAEKJOON/DFS/BOJ_1189_컴백홈.py
@@ -2,17 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
+r, c, k = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(r)]
+
+answer = 0
+
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 
-def dfs(x, y, depth):
+def dfs(x, y, dep):
     global answer
 
-    if depth == k and x == 0 and y == c - 1:
+    if dep == k and x == 0 and y == c - 1:
         answer += 1
     else:
-
         board[x][y] = 'T'
 
         for i in range(4):
@@ -21,12 +25,8 @@ def dfs(x, y, depth):
 
             if 0 <= nx < r and 0 <= ny < c and board[nx][ny] == '.':
                 board[nx][ny] = 'T'
-                dfs(nx, ny, depth + 1)
+                dfs(nx, ny, dep + 1)
                 board[nx][ny] = '.'
 
-
-r, c, k = map(int, input().split())
-board = [list(input().rstrip()) for _ in range(r)]
-answer = 0
-dfs(r-1,0,1)
+dfs(r-1, 0, 0)
 print(answer)
