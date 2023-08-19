a507fcf0348eec17bd0f65d2ba479772fa6431d7
ChanYoungHong <cksdud9883@naver.com>
2023-08-19 13:55:22 +0900

    백트래킹 - 복습2

--- BAEKJOON/부르트포스/BOJ_14620_꽃길.py
+++ BAEKJOON/부르트포스/BOJ_14620_꽃길.py
@@ -5,13 +5,14 @@ input = sys.stdin.readline
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def check(x,y,visited):
 
+def check(x, y, visited):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if (nx,ny) in visited:
+        # 방문을 했으면 false를 반환한다.
+        if (nx, ny) in visited:
             return False
     return True
 
@@ -20,9 +21,12 @@ def dfs(visited, total):
 
     global answer
 
+    # 무슨 조건이 있었음 - 최소비용으로 구하기 위한 조건인 듯
     if total >= answer:
         return
 
+    # 꽃 3개 즉 15개의 지점이 다 심겼으면 종료하기
+    # dfs를 계속 돌리면서 최소비용을 구하기
     if len(visited) == 15:
         answer = min(total, answer)
 
@@ -31,23 +35,22 @@ def dfs(visited, total):
         for i in range(1, n-1):
             for j in range(1, n-1):
 
+                # 주변에 좌표가 없는 지 확인한다.
                 if check(i,j,visited) and (i,j) not in visited:
-                    temp =[(i,j)]
+                    temp = [(i,j)]
                     temp_cost = board[i][j]
 
-                    for idx in range(4):
-
-                        nx = i + dx[idx]
-                        ny = j + dy[idx]
+                    for kk in range(4):
+                        nx = i + dx[kk]
+                        ny = j + dy[kk]
                         temp.append((nx,ny))
                         temp_cost += board[nx][ny]
+
                     dfs(visited + temp, total + temp_cost)
 
-
-
 n = int(input())
-answer = int(1e9)
+answer = 1e9
 board = [list(map(int, input().split())) for _ in range(n)]
 dfs([], 0)
-
 print(answer)
+
