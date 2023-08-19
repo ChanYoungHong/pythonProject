1052a658bbe797ff6aeb79bba0aa8c54fe06d178
ChanYoungHong <cksdud9883@naver.com>
2023-08-19 15:47:42 +0900

    시뮬레이션 - 복습2

--- BAEKJOON/구현/BOJ_10709_기상캐스터.py
+++ BAEKJOON/구현/BOJ_10709_기상캐스터.py
@@ -2,40 +2,30 @@ import sys
 
 input = sys.stdin.readline
 
-
-'''
-1. 구름이 1분 지날 때마다 1키로씩 이동한다
-2. 
-
-'''
-
 n,m = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(n)]
 
-t_cloud = 1
+
+t_cloud = 0
 is_cloud = False
+
 
 for i in range(n):
     for j in range(m):
 
         if board[i][j] == 'c':
             board[i][j] = 0
-            t_cloud = 1
             is_cloud = True
+            t_cloud = 1
+
         elif board[i][j] == '.' and is_cloud == False:
             board[i][j] = -1
 
         elif board[i][j] == '.' and is_cloud == True:
             board[i][j] = t_cloud
+            is_cloud = True
             t_cloud += 1
 
-    is_cloud = False
     t_cloud = 1
+    is_cloud = False
     print(*board[i])
-
-
-
-
-
-
-
