8b317defc78632ea759d766b1b35a00caa1ca74c
ChanYoungHong <cksdud9883@naver.com>
2023-08-23 14:00:20 +0900

    너비우선탐색 - 문제

--- BAEKJOON/백트래킹/BOJ_2589_보물섬.py
+++ BAEKJOON/백트래킹/BOJ_2589_보물섬.py
@@ -3,58 +3,26 @@ from collections import deque
 
 input = sys.stdin.readline
 
-'''
-1. 평소에는 오른쪽 밑으로 가게 했는데 
-2. 일단 최댓값을 구하면 안 되나?
-
-'''
-
 n, m = map(int, input().split())
-board = [list(input().rstrip()) for _ in range(n)]
+board = [list(input()) for _ in range(n)]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-# def dfs(x,y):
-#
-#     global cnt
-#     cnt = 1
-#
-#     for i in range(4):
-#         nx = x + dx[i]
-#         ny = y + dy[i]
-#
-#         if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 'L':
-#             board[nx][ny] = 'W'
-#             dfs(nx,ny)
-#             cnt += 1
-#
-#     return cnt
-#
-# cnt = 0
-# res = []
-# for i in range(n):
-#     for j in range(m):
-#
-#         if board[i][j] == 'L':
-#             board[i][j] = 'W'
-#             res.append(dfs(i,j))
-#
-# print(max(res))
-
-visited = [[0] * m for _ in range(n)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 
-def bfs(i, j):
+def bfs(i,j):
+
     q = deque()
-    q.append((i, j))
+    q.append((i,j))
 
+    visited = [[0] * m for _ in range(n)]
     visited[i][j] = 1
     cnt = 0
 
     while q:
 
-        x, y = q.popleft()
+        x,y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -63,9 +31,9 @@ def bfs(i, j):
                 if board[nx][ny] == 'L' and visited[nx][ny] == 0:
                     visited[nx][ny] = visited[x][y] + 1
                     cnt = max(cnt, visited[nx][ny])
-                    q.append((nx, ny))
+                    q.append((nx,ny))
 
-    return cnt
+    return cnt - 1
 
 
 result = 0
@@ -73,6 +41,6 @@ for i in range(n):
     for j in range(m):
 
         if board[i][j] == 'L':
-            result = max(result, bfs(i, j))
+            result = max(result, bfs(i,j))
 
 print(result)
