732fc03051c88c21afef62c78493fbfca49a3a87
ChanYoungHong <cksdud9883@naver.com>
2023-08-31 11:38:50 +0900

    구현문제 - 복습

--- BAEKJOON/구현/BOJ_2852_NBA농구.py
+++ BAEKJOON/구현/BOJ_2852_NBA농구.py
@@ -2,52 +2,53 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-
-
-# 시간 -> 초로 환산
+# 시간 -> 숫자로 환산
 def timeToNum(strTime):
+    hour, min = map(int, strTime.split(':'))
+    return hour * 60 + min
 
-    min, sec = map(int, strTime.split(":"))
-    return min * 60 + sec
 
-# 초 -> 시간으로 환산
-def numToTime(time):
-    min = str(time // 60)
-    sec = str(time % 60)
+# 숫자 -> 시간으로 환산
+def numToTime(ttime):
+
+    hour = str(ttime // 60)
+    min = str(ttime % 60)
+
+    if len(hour) == 1:
+        hour = '0'+hour
 
     if len(min) == 1:
         min = '0' + min
-
-    if len(sec) == 1:
-        sec = '0' + sec
-
-    return min + ':' + sec
+    return hour + ':' + min
 
 scoreDict = {}
 
-for i in range(n):
+n = int(input())
+for _ in range(n):
     teamNo, strTime = input().split()
     scoreDict[timeToNum(strTime)] = int(teamNo)
 
-team1Win = 0
-team2Win = 0
+team1win = 0
+team2win = 0
 
-teamScore1 = 0
-teamScore2 = 0
+team1score = 0
+team2score = 0
 
-for k in range(timeToNum("48:00")):
+# print(scoreDict)
+for i in range(timeToNum('48:00')):
 
-    if k in scoreDict:
-        if scoreDict[k] == 1:
-            teamScore1 += 1
+    if i in scoreDict:
+        if scoreDict[i] == 1:
+            team1score += 1
         else:
-            teamScore2 += 1
+            team2score += 1
 
-    if teamScore1 > teamScore2:
-        team1Win += 1
-    elif teamScore1 < teamScore2:
-        team2Win += 1
+    if team1score > team2score:
+        team1win += 1
+    elif team1score < team2score:
+        team2win += 1
 
-print(numToTime(team1Win))
-print(numToTime(team2Win))
+
+print(numToTime(team1win))
+print(numToTime(team2win))
+
