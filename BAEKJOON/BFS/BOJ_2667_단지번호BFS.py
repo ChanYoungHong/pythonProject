78ff1862aa14d01d6f6ccde8fdf2fd72b118d67c
ChanYoungHong <cksdud9883@naver.com>
2023-09-01 13:59:52 +0900

    bfs 문제복습

--- BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
+++ BAEKJOON/BFS/BOJ_2667_단지번호BFS.py
@@ -1,6 +1,7 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
 n = int(input())
 board = [list(map(int, input().rstrip())) for _ in range(n)]
@@ -8,7 +9,6 @@ board = [list(map(int, input().rstrip())) for _ in range(n)]
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-cnt = 0
 
 def bfs(x,y):
 
@@ -16,24 +16,27 @@ def bfs(x,y):
 
     q = deque()
     q.append((x,y))
-
     cnt = 1
+
     while q:
 
         x,y = q.popleft()
 
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
+            nx = x + dx[i]
+            ny = y + dy[i]
 
             if 0 <= nx < n and 0 <= ny < n:
                 if board[nx][ny] == 1:
                     board[nx][ny] = 0
-                    cnt += 1
                     q.append((nx,ny))
+                    cnt += 1
+
 
     return cnt
 
+
+cnt = 0
 res = []
 for i in range(n):
     for j in range(n):
@@ -45,9 +48,5 @@ for i in range(n):
 
 print(len(res))
 res.sort()
-
-for k in res:
-    print(k)
-
-
-
+for i in res:
+    print(i)
