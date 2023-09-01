f543945a67cf5a22e319cf0df3344986a108c120
ChanYoungHong <cksdud9883@naver.com>
2023-09-01 18:11:31 +0900

    bfs 골드3 - 복습하기

--- BAEKJOON/BFS/BOJ_16236_아기상어.py
+++ BAEKJOON/BFS/BOJ_16236_아기상어.py
@@ -1,94 +1,75 @@
-'''
-1. 시간복잡도 : bfs를 써서 아무렇게나 짜면 됨, 왜 bfs를 써야하나 ?
-2. 아이디어 : 조건 그대로 코드를 짜면 될 듯 ..
-더 이상 먹을 수 있는 물고기가 공간에 없다면 아기 상어는 엄마 상어에게 도움을 요청한다.
-먹을 수 있는 물고기가 1마리라면, 그 물고기를 먹으러 간다.
-먹을 수 있는 물고기가 1마리보다 많다면, 거리가 가장 가까운 물고기를 먹으러 간다.
-거리는 아기 상어가 있는 칸에서 물고기가 있는 칸으로 이동할 때, 지나야하는 칸의 개수의 최솟값이다.
-거리가 가까운 물고기가 많다면, 가장 위에 있는 물고기, 그러한 물고기가 여러마리라면, 가장 왼쪽에 있는 물고기를 먹는다.
-'''
-
-'''
-
-1 추가적인 디테일 -> 아기상어가 먹은 후에는 그 자리는 0이 되어야 함.
-2 일단 물고기가 있는지 없는지 확인을 해야 함
-
-'''
-from collections import deque
 import sys
+from collections import deque
 
-input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-babyshark = 2
-dist = 0
-move_num = 0
-eat = 0
+board = [list(map(int, input().split())) for _ in range(n)]
 
-INF = sys.maxsize
+s_x, s_y = 0, 0
+eat_cnt = 0
+s_size = 2
 
-size = 0
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-# 추가
-sx, sy = 0,0
-
+# 아기상어 위치 좌표 그리고 움직이므로 0으로 초기화
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 9:
-            graph[i][j] = 0
-            sx, sy = i, j
-            break
 
-        # if graph[i][j] == 0:
-        #     print(0)
-        #     # 종료 시키기
-        # elif graph[i][j] == 9:
-        #     q.append((i, j))
+        if board[i][j] == 9:
+            s_x, s_y = i, j
+            board[i][j] = 0
+
+
 def bfs():
+    global s_x, s_y, eat_cnt, s_size
 
-    q = deque()
-    q.append((sx, sy, 0))
     visited = [[False] * n for _ in range(n)]
-    flag = 1e9
-    fish = []
+    q = deque([(s_x, s_y, 0)])
+    candi = []
 
     while q:
-        global babyshark, dist, move_num, move, eat
 
-        x, y, count = q.popleft()
-
-        if count > flag:
-            break
+        x, y, time = q.popleft()
+        visited[x][y] = True
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
+                if board[nx][ny] <= s_size:
+                    q.append((nx, ny, time + 1))
+                    visited[nx][ny] = True
 
-            if graph[nx][ny] > size or visited[nx][ny]:
-                continue
+                # 먹을 수 있는 물고기 범위 추가
+                if 0 < board[nx][ny] < s_size:
+                    candi.append((nx, ny, time + 1))
 
-            ''' 아기상어 레벨이 2이라는 것을 표시를 해줘야 함.. 어떻게 함 ? '''
+    # 먹을 수 있는 물고기가 하나도 없을 경우
+    if not candi:
+        return None
 
-            if graph[nx][ny] != 0 and graph[nx][ny] < size:
-                fish.append((nx, ny, count+1))
-                flag = count
-            visited[nx][ny] = True
-            q.append((nx,ny,count + 1))
+    # 가장 가까운 물고기로 정렬하기
+    candi.sort(key=lambda x: (x[2], x[0], x[1]))
+    return candi[0]
 
-            # elif graph[nx][ny] == babyshark:
-            #     '''지나가는 것을 어떻게 표현 함 ?? '''
-            # elif graph[nx][ny] > babyshark:
-            #     continue
 
-    if len(fish) > 0:
-        fish.sort()
-        x, y, move = fish[0][[0], fish[0][1], fish[0][2]
-        move_num += move
-        eat += 1
-        graph[0][0] = 0
+total_time = 0
+while True:
+    result = bfs()
+
+    if result is None:
+        break
+
+    x, y, time = result
+    total_time += time
+
+    # 자신의 크기와 같은 수의 물고리를 먹으면 크기가 1증가 하는 코드
+    if eat_cnt == s_size:
+        s_size += 1
+        eat_cnt = 0
+
+    s_x, s_y = x, y
+    board[x][y] = 0
+
+print(total_time)
