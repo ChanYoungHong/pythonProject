13e60123aa610c68766d1f9a1a142b5b0d2e0658
ChanYoungHong <cksdud9883@naver.com>
2023-09-05 14:12:14 +0900

    dp문제 풀이

--- BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11053_가장긴증가하는부분수열.py
@@ -11,7 +11,7 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1] *n
+dp = [1] * n
 
 for i in range(n):
     for j in range(i):
@@ -19,7 +19,6 @@ for i in range(n):
         if arr[i] > arr[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
-# print(dp)
 print(max(dp))
-
+print(dp)
 
