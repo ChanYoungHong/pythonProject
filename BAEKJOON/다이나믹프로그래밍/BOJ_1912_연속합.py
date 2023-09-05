df7b157a943ba1ea3aee5a1ef1e356f0f15fe9c8
ChanYoungHong <cksdud9883@naver.com>
2023-09-05 14:23:28 +0900

    옛날에 풀었던 dp문제 복습 ...

--- BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 
 '''
@@ -14,17 +13,12 @@ input = sys.stdin.readline
 '''
 
 n = int(input())
-
 arr = list(map(int, input().split()))
 
 dp = [arr[0]]
 
 for i in range(n-1):
-    dp.append(max(dp[i]+arr[i+1], arr[i+1]))
+    dp.append(max(dp[i] + arr[i+1], arr[i+1]))
 
+print(dp)
 print(max(dp))
-
-
-
-
-
