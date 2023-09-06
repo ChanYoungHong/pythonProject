0be4643796f0b188c77fb4608114b5c2f34e4385
ChanYoungHong <cksdud9883@naver.com>
2023-09-06 14:44:42 +0900

    dp - 문제복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_1912_연속합.py
@@ -18,7 +18,7 @@ arr = list(map(int, input().split()))
 dp = [arr[0]]
 
 for i in range(n-1):
-    dp.append(max(dp[i] + arr[i+1], arr[i+1]))
 
-print(dp)
+    dp.append(max(dp[i]+arr[i+1], arr[i+1]))
+
 print(max(dp))
