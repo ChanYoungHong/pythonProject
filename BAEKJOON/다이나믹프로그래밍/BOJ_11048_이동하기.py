be8a4b3246e2e4e21a935368f2b5feefb66412f5
ChanYoungHong <cksdud9883@naver.com>
2023-09-07 19:56:44 +0900

    dp - 다른 관점을 접근하기

--- BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
@@ -3,13 +3,23 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[0] * (m+1) for _ in range(n+1)]
+
+
+
 
 for i in range(n):
     for j in range(m):
 
-        dp[i][j] = graph[i][j] + max(dp[i-1][j], dp[i][j-1])
+        dp[i][j] = arr[i][j] + max(dp[i-1][j], dp[i][j-1])
+
+
+# for i in range(n+1):
+#     for j in range(m+1):
+#
+#         print(dp[i][j], end = ' ')
+#     print()
 
 print(dp[n-1][m-1])
