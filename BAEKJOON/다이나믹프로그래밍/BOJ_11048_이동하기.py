d6456e62ba8c353f7c2afaa2c39b96c937b7224d
ChanYoungHong <cksdud9883@naver.com>
2023-09-08 12:37:44 +0900

    dp - 문제복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_11048_이동하기.py
@@ -3,23 +3,15 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+
+board = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[0] * (m+1) for _ in range(n+1)]
-
-
-
 
 for i in range(n):
     for j in range(m):
 
-        dp[i][j] = arr[i][j] + max(dp[i-1][j], dp[i][j-1])
-
-
-# for i in range(n+1):
-#     for j in range(m+1):
-#
-#         print(dp[i][j], end = ' ')
-#     print()
+        dp[i][j] = board[i][j] + max(dp[i-1][j], dp[i][j-1])
 
 print(dp[n-1][m-1])
+print(dp)
