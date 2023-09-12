826c844243daf5a2c12adbb08f9240c868a11a13
ChanYoungHong <cksdud9883@naver.com>
2023-09-12 12:38:06 +0900

    구현문제 - 복습

--- BAEKJOON/구현/BOJ_2578_빙고.py
+++ BAEKJOON/구현/BOJ_2578_빙고.py
@@ -4,23 +4,24 @@ input = sys.stdin.readline
 
 board = [list(map(int, input().split())) for _ in range(5)]
 mc = []
-for _ in range(5):
+
+for i in range(5):
     mc += list(map(int, input().split()))
+
 
 def check():
     bingo = 0
 
-    # 가로 검사
-    for q in board:
-        if q.count(0) == 5:
+    # 가로검사
+    for i in board:
+        if i.count(0) == 5:
             bingo += 1
 
-    # 세로 검사
-    for i in range(5):
+    # 세로검사
+    for k in range(5):
         t = 0
-        for j in range(5):
-
-            if board[j][i] == 0:
+        for z in range(5):
+            if board[z][k] == 0:
                 t += 1
 
         if t == 5:
@@ -28,8 +29,8 @@ def check():
 
     # 왼쪽 대각선 검사
     d1 = 0
-    for k in range(5):
-        if board[k][k] == 0:
+    for qq in range(5):
+        if board[qq][qq] == 0:
             d1 += 1
 
     if d1 == 5:
@@ -37,8 +38,8 @@ def check():
 
     # 오른쪽 대각선 검사
     d2 = 0
-    for z in range(5):
-        if board[z][4-z] == 0:
+    for v in range(5):
+        if board[v][4 - v] == 0:
             d2 += 1
 
     if d2 == 5:
@@ -49,12 +50,13 @@ def check():
 
 cnt = 0
 for i in range(25):
-    for v in range(5):
-        for b in range(5):
+    for j in range(5):
+        for k in range(5):
 
-            if mc[i] == board[v][b]:
-                board[v][b] = 0
+            if mc[i] == board[j][k]:
+                board[j][k] = 0
                 cnt += 1
+
 
     if cnt >= 12:
         result = check()
