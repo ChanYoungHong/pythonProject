77c60cfe55c14667326eeef021077ae375ee42ee
ChanYoungHong <cksdud9883@naver.com>
2023-09-12 20:07:42 +0900

    bfs - 새로운 문제

--- BAEKJOON/BFS/BOJ_1697_숨바꼭질.py
+++ BAEKJOON/BFS/BOJ_1697_숨바꼭질.py
@@ -1,67 +1,42 @@
-# from collections import deque
-# import sys
-#
-# input = sys.stdin.readline
-#
-# n, m = map(int, input().split())
-# cnt = 0
-#
-# oper = [-1,1,2]
-#
-# def bfs():
-#
-#     global cnt
-#
-#     q = deque()
-#     q.append()
-#
-#     while q:
-#         x,y = q.popleft()
-#
-#         for i in range(3):
-#             if x == y:
-#                 break
-#
-#             elif x < y:
-#                 nx = x + oper[i]
-#                 cnt += 1
-#                 q.append(nx)
-#                 if i == 2:
-#                     nx = x * oper[i]
-#                     cnt += 1
-#                     q.append(nx)
-#
-# print(bfs())
-
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-cnt = 0
+'''
+1. 시간복잡도
+2. 어떻게 bfs로 풀 수 있을까?
+3. 가로 선 상의 기준으로 bfs를 돌리나 ?? (00) (01) (02) (03) 이런 식으로 ?
+4. 하나의 노드가 얼마나 깊이까지 가는지 모두 확인해서 최단시간을 찾으려면 다 탐색해야하니
+bfs가 dfs보다 적합하다고 판다
+5. 
+'''
 
-maxx = 10 ** 5
-array = [0] * (maxx + 1)
 
+# dx = [1,-1,0,0]
+# dy = [0,0,1,-1]
+#
+#
 
 def bfs():
-    global cnt
 
     q = deque()
     q.append(n)
 
     while q:
         x = q.popleft()
-
-        if x == m:
-            print(array[x])
+        if x == k:
+            print(dist[x])
             break
 
-        for j in (x + 1, x - 1, x * 2):
-            if 0 <= j <= maxx and not array[j]:
-                array[j] = array[x] + 1
-                q.append(j)
+        for nx in (x - 1, x + 1, x * 2):
+            if 0 <= nx <= MAX and not dist[nx]:
+                dist[nx] = dist[x] + 1
+                q.append(nx)
 
+
+MAX = 10 ** 5
+n, k = map(int, input().split())
+dist = [0] * (MAX + 1)
 
 bfs()
