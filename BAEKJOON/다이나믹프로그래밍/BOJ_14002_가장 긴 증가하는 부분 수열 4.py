e5d2a47f66edcca53781165e6cb0e876660f1c64
ChanYoungHong <cksdud9883@naver.com>
2023-09-14 16:17:48 +0900

    dp문제 - 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_14002_가장 긴 증가하는 부분 수열 4.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_14002_가장 긴 증가하는 부분 수열 4.py
@@ -2,11 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-'''
-1. 1 < n < 1000 사이에 있음 - 시간 복잡도 고려하기 
-
-'''
-
 n = int(input())
 arr = list(map(int, input().split()))
 
@@ -16,18 +11,16 @@ for i in range(n):
     for j in range(i):
 
         if arr[i] > arr[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+            dp[i] = max(dp[i], dp[j] + 1)
 
 # print(dp)
 
 order = max(dp)
 res = []
+for z in range(n - 1, -1, -1):
 
-for j in range(n-1, -1, -1):
-
-    # print('j : ', j, end=' ')
-    if order == dp[j]:
-        res.append(arr[j])
+    if order == dp[z]:
+        res.append(arr[z])
         order -= 1
 
 print(len(res))
