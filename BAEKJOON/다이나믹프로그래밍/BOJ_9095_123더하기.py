b1133615a6a9f881b1fda79777532c60dd473f70
ChanYoungHong <cksdud9883@naver.com>
2023-09-15 17:45:50 +0900

    dp문제 복습

--- BAEKJOON/다이나믹프로그래밍/BOJ_9095_123더하기.py
+++ BAEKJOON/다이나믹프로그래밍/BOJ_9095_123더하기.py
@@ -10,10 +10,10 @@ dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in range(4, 12):
-    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
-for i in range(n):
-    # a = int(input)
+for i in range(4, len(dp)):
+    dp[i] = (dp[i-3] + dp[i-2] + dp[i-1])
+
+print(dp[7])
+for _ in range(n):
     print(dp[int(input())])
-
