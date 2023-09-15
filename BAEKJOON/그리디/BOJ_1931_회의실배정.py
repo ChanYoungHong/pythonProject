3e54c02effdd7ac6bc83a31f09064151411d96bd
ChanYoungHong <cksdud9883@naver.com>
2023-09-15 17:33:06 +0900

    구현문제 - 복습

--- BAEKJOON/그리디/BOJ_1931_회의실배정.py
+++ BAEKJOON/그리디/BOJ_1931_회의실배정.py
@@ -1,41 +1,36 @@
-'''
-1. 시간복잡도 -
-2. 아이디어 - 람다 사용을 할 줄 알아야 함
-'''
-
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-result = []
 
-for i in range(n):
+res = []
+for _ in range(n):
     a, b = map(int, input().split())
-    result.append((a,b))
+    res.append((a, b))
 
-result = sorted(result, key=lambda x:x[0])
-result = sorted(result, key=lambda x:x[1])
+res = sorted(res, key=lambda x: x[0])
+res = sorted(res, key=lambda x: x[1])
+
+# print(res)
 
 last = 0
 cnt = 0
-for i,j in result:
+for i, j in res:
+
     if i >= last:
         last = j
         cnt += 1
 
 print(cnt)
-# for _ in range(n):
-#     a, b = map(int, input().split())
-#     result.append((a,b))
-#
-# result.sort()
-#
-# print(result)
-# answer = []
-# for i in range(n):
-#     temp = abs(result[i][1] - result[i][0])
-#     answer.append(temp)
-#
-# print(answer)
 
+#
+#     # last = j
+#     #
+#     # if last > i:
+#     #     continue
+#     # else:
+#     #     last = j
+#     #     cnt += 1
+#
+# print(cnt)
