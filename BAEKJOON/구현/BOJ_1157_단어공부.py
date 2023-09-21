8617b50c5f065dec85114c7a8db3b1d3bbb9466e
ChanYoungHong <cksdud9883@naver.com>
2023-09-21 17:07:45 +0900

    구현문제 복습

--- BAEKJOON/구현/BOJ_1157_단어공부.py
+++ BAEKJOON/구현/BOJ_1157_단어공부.py
@@ -1,27 +1,56 @@
-'''
-
-1. 시간복잡도 -
-2. 아이디어  - 리스트를 만들어서 각각 인덱스를 돌려서 검사하는 방법
--> count() 함수를 사용해서 가장 큰 수를 뽑은 후 그 수를 대문자로 바꿔주기
-
-
-'''
-
-import sys
-
+# import sys
+#
 # input = sys.stdin.readline
+# '''
+# 1. 각 알파벳의 개수를 temp에 저장
+# 2. count() 함수로 for문 돌려서 찾아내기
+# 3.
+#
+# '''
+# word = input().rstrip()
+# word = word.upper()
+#
+# print(word)
+# temp = {}
+#
+# for i in word:
+#
+#     if i not in temp:
+#         temp[i] = 0
+#     temp[i] += 1
+#
+# print(temp)
+# big = 0
+#
+# # # hi = []
+# # # print('test : ', max(temp))
+# # # hi.append(max(temp))
+# # # print(hi)
+# # # print(len(hi))
+# #
+# # test = max(temp, key=temp.get)
+# #
+# # print(test)
+#
+# # tmp = 0
+# # for a,b in temp.items():
+# #
+# #     if b > tmp:
+# #         tmp = b
+# #         print(tmp)
+#
+#
 
-words = input().upper()
-array = list(set(words))
+word = input().rstrip().upper()
+array = list(set(word))
 
 cnt_array = []
 for i in array:
-    cnt = words.count(i)
-    cnt_array.append(cnt)
+    cnt_array.append(word.count(i))
+
 
 if cnt_array.count(max(cnt_array)) > 1:
     print('?')
 else:
     max_index = cnt_array.index(max(cnt_array))
     print(array[max_index])
-
