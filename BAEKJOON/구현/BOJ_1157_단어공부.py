932ff7afbdd512d9cbaa4a169f130d1679f356ac
ChanYoungHong <cksdud9883@naver.com>
2023-09-22 10:57:26 +0900

    어제 풀었던 문제 복습

--- BAEKJOON/구현/BOJ_1157_단어공부.py
+++ BAEKJOON/구현/BOJ_1157_단어공부.py
@@ -1,56 +1,21 @@
-# import sys
-#
-# input = sys.stdin.readline
-# '''
-# 1. 각 알파벳의 개수를 temp에 저장
-# 2. count() 함수로 for문 돌려서 찾아내기
-# 3.
-#
-# '''
-# word = input().rstrip()
-# word = word.upper()
-#
-# print(word)
-# temp = {}
-#
-# for i in word:
-#
-#     if i not in temp:
-#         temp[i] = 0
-#     temp[i] += 1
-#
-# print(temp)
-# big = 0
-#
-# # # hi = []
-# # # print('test : ', max(temp))
-# # # hi.append(max(temp))
-# # # print(hi)
-# # # print(len(hi))
-# #
-# # test = max(temp, key=temp.get)
-# #
-# # print(test)
-#
-# # tmp = 0
-# # for a,b in temp.items():
-# #
-# #     if b > tmp:
-# #         tmp = b
-# #         print(tmp)
-#
-#
+import sys
 
-word = input().rstrip().upper()
-array = list(set(word))
+input = sys.stdin.readline
 
-cnt_array = []
-for i in array:
-    cnt_array.append(word.count(i))
+word = input().rstrip()
+word = word.upper()
 
+arr = list(set(word))
 
-if cnt_array.count(max(cnt_array)) > 1:
+print('arr : ', arr)
+check = []
+for i in arr:
+    check.append(word.count(i))
+
+print('check : ', check)
+if check.count(max(check)) > 1:
     print('?')
+
 else:
-    max_index = cnt_array.index(max(cnt_array))
-    print(array[max_index])
+    max_index = check.index(max(check))
+    print(arr[max_index])
