3d9bc9b4648a7c36ce0d4bd00e6b3167d2659d78
ChanYoungHong <cksdud9883@naver.com>
2023-09-22 16:13:12 +0900

    구현문제 복습

--- BAEKJOON/구현/BOJ_2852_NBA농구.py
+++ BAEKJOON/구현/BOJ_2852_NBA농구.py
@@ -2,31 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
-# 시간 -> 숫자로 환산
-def timeToNum(strTime):
-    hour, min = map(int, strTime.split(':'))
+
+# 시간 -> 숫자로
+def timeToNum(str):
+    hour, min = map(int, str.split(':'))
     return hour * 60 + min
 
 
-# 숫자 -> 시간으로 환산
-def numToTime(ttime):
-
-    hour = str(ttime // 60)
-    min = str(ttime % 60)
+def numToTime(nnum):
+    hour = str(nnum // 60)
+    min = str(nnum % 60)
 
     if len(hour) == 1:
-        hour = '0'+hour
+        hour =  '0' + hour
 
     if len(min) == 1:
-        min = '0' + min
+        min =  '0' + min
+
     return hour + ':' + min
 
-scoreDict = {}
-
 n = int(input())
+teamDict = {}
 for _ in range(n):
-    teamNo, strTime = input().split()
-    scoreDict[timeToNum(strTime)] = int(teamNo)
+    teamNo, timeStr = map(str, input().rstrip().split())
+    teamDict[timeToNum(timeStr)] = int(teamNo)
+
 
 team1win = 0
 team2win = 0
@@ -34,11 +34,12 @@ team2win = 0
 team1score = 0
 team2score = 0
 
-# print(scoreDict)
+# print(teamDict)
+# print(timeToNum('48:00'))
 for i in range(timeToNum('48:00')):
 
-    if i in scoreDict:
-        if scoreDict[i] == 1:
+    if i in teamDict:
+        if teamDict[i] == 1:
             team1score += 1
         else:
             team2score += 1
@@ -48,7 +49,8 @@ for i in range(timeToNum('48:00')):
     elif team1score < team2score:
         team2win += 1
 
-
 print(numToTime(team1win))
 print(numToTime(team2win))
+
+
 
