fe2246f6fad55288d4e5186ee73558c40c3f46d5
ChanYoungHong <cksdud9883@naver.com>
2023-09-22 16:39:42 +0900

    bfs 문제복습

--- BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
+++ BAEKJOON/BFS/BOJ_2178_BFS_미로탐색.py
@@ -3,19 +3,21 @@ from collections import deque
 
 input = sys.stdin.readline
 
-
 n,m = map(int, input().split())
 board = [list(map(int, input().rstrip())) for _ in range(n)]
 
 dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+dy = [0,0,-1,1]
 
 def bfs(x,y):
 
     q = deque()
     q.append((x,y))
 
+    board[x][y] = 1
+
     while q:
+
         x,y = q.popleft()
 
         for i in range(4):
@@ -24,10 +26,19 @@ def bfs(x,y):
 
             if 0 <= nx < n and 0 <= ny < m:
                 if board[nx][ny] == 1:
+                    # board[nx][ny] = 0
                     board[nx][ny] = board[x][y] + 1
                     q.append((nx,ny))
 
     return board[-1][-1]
 
+# for i in range(n):
+#     for j in range(m):
+#
+#         if board[i][j] == 1:
+#             board[i][j] = 0
+#             bfs(i,j)
 
 print(bfs(0,0))
+
+
