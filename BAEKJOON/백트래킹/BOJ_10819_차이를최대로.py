9c66e530d1b4e84408db06ba8e8d50abb0984173
ChanYoungHong <cksdud9883@naver.com>
2023-10-06 16:28:23 +0900

    백트래킹 문제복습

--- BAEKJOON/백트래킹/BOJ_10819_차이를최대로.py
+++ BAEKJOON/백트래킹/BOJ_10819_차이를최대로.py
@@ -1,51 +1,18 @@
-# import sys
-#
-# input = sys.stdin.readline
-#
-# n = int(input())
-# nums = list(map(int, input().split()))
-# visited = [False] * (n + 1)
-# result = 0
-# res = []
-# Nsum = 0
-#
-#
-# def dfs(dep):
-#     global result
-#
-#     if dep == n:
-#         Nsum = 0
-#         for i in range(n - 1):
-#             Nsum += abs(res[i] - res[i + 1])
-#         result = max(result, Nsum)
-#
-#     for i in range(n):
-#
-#         if visited[i] == False:
-#             visited[i] = True
-#             res.append(nums[i])
-#             dfs(dep + 1)
-#             res.pop()
-#             visited[i] = False
-#
-#
-# dfs(0)
-# print(result)
-
-
+import sys
 from itertools import permutations
 
+input = sys.stdin.readline
+
+
 n = int(input())
-nums = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 
-ans = 0
+res = 0
+for per in permutations(arr):
 
-for per in permutations(nums):
-    res = 0
+    tmp = 0
     for i in range(n-1):
-        res += abs(per[i] - per[i+1])
-        print(per[i], per[i+1])
+        tmp += abs(per[i] - per[i+1])
+    res = max(res, tmp)
 
-    ans = max(ans, res)
-
-print(ans)
+print(res)
