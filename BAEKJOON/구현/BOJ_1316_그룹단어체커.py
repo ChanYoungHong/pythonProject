03ba05701562ae7369f298ac3f6e79cd2cc88b21
ChanYoungHong <cksdud9883@naver.com>
2023-10-26 13:00:34 +0900

    구현문제 - 복습

--- BAEKJOON/구현/BOJ_1316_그룹단어체커.py
+++ BAEKJOON/구현/BOJ_1316_그룹단어체커.py
@@ -3,23 +3,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+answer = 0
 
-cnt = 0
 for _ in range(n):
-
     word = input().rstrip()
 
-    t = 0
-    for i in range(len(word) - 1):
+    cnt = 0
+    for i in range(len(word)-1):
 
         if word[i] != word[i+1]:
             new_word = word[i+1:]
 
             if new_word.count(word[i]) > 0:
-                t += 1
+                cnt += 1
 
-    if t == 0:
-        cnt += 1
+    if cnt == 0:
+        answer += 1
 
-print(cnt)
-
+print(answer)
