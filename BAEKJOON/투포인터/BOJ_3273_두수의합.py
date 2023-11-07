e52c5fa3e2695812a86258e5a8b13c8b19d9b1a4
ChanYoungHong <cksdud9883@naver.com>
2023-11-07 20:43:58 +0900

    투 포인터 문제 풀이완료

---
+++ BAEKJOON/투포인터/BOJ_3273_두수의합.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+
+
+arr.sort()
+
+start = 0
+end = len(arr) - 1
+cnt = 0
+
+while start < end:
+
+    if arr[start] + arr[end] > m:
+        end -= 1
+    elif arr[start] + arr[end] < m:
+        start += 1
+    else:
+        cnt += 1
+        start += 1
+        end -= 1
+
+print(cnt)
