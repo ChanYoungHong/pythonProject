ed056bdfcfbac3310e96f4b6277584f2882e17e7
ChanYoungHong <cksdud9883@naver.com>
2023-11-08 14:09:11 +0900

    bfs - 어제문제 복습

--- BAEKJOON/BFS/BOJ_1012_유기농배추.py
+++ BAEKJOON/BFS/BOJ_1012_유기농배추.py
@@ -1,51 +1,54 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
+
+t = int(input())
+
 '''
-m - 가로
-n - 세로
+m은 가로, n은 세로
 '''
 
-dx = [1,-1,0,0]
+dx = [-1,1,0,0]
 dy = [0,0,1,-1]
 
-def bfs(x,y):
+def bfs(a,b):
 
     q = deque()
-    q.append((x,y))
+    q.append((a,b))
 
     while q:
 
         x,y = q.popleft()
 
         for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            nx = dx[i] + x
-            ny = dy[i] + y
             if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
-                    q.append((nx, ny))
+                if board[nx][ny] == 1:
+                    board[nx][ny] = 0
+                    q.append((nx,ny))
 
 
-t = int(input())
-
-for i in range(t):
+# board = []
+for _ in range(t):
 
     m,n,k = map(int, input().split())
-    graph = [[0] * m for _ in range(n)]
+    board = [[0] * m for _ in range(n)]
     cnt = 0
 
-    for j in range(k):
+    for _ in range(k):
+
         a,b = map(int, input().split())
-        graph[b][a] = 1
+        board[b][a] = 1
+
 
     for i in range(n):
         for j in range(m):
 
-            if graph[i][j] == 1:
+            if board[i][j] == 1:
                 bfs(i,j)
                 cnt += 1
 
     print(cnt)
-
