7b72b5b9eeb111a3ee76c1bd9491f872816b5135
ChanYoungHong <cksdud9883@naver.com>
2023-11-08 16:00:07 +0900

    투 포인터 문제복습

--- BAEKJOON/투포인터/BOJ_3273_두수의합.py
+++ BAEKJOON/투포인터/BOJ_3273_두수의합.py
@@ -4,24 +4,24 @@ input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-m = int(input())
+ans = int(input())
 
 
 arr.sort()
-
 start = 0
 end = len(arr) - 1
 cnt = 0
 
 while start < end:
 
-    if arr[start] + arr[end] > m:
+    if arr[start] + arr[end] > ans:
         end -= 1
-    elif arr[start] + arr[end] < m:
+    elif arr[start] + arr[end] < ans:
         start += 1
     else:
         cnt += 1
         start += 1
         end -= 1
+
 
 print(cnt)
