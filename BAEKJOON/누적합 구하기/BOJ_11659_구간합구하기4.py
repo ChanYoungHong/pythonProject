cd35f9966fedd693ad7a4056508c25e2ad118bea
ChanYoungHong <cksdud9883@naver.com>
2023-11-08 16:36:33 +0900

    누적합 문제풀이

--- BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
+++ BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
@@ -2,17 +2,31 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+'''
+n < 100000 이니깐 nlogn을 사용해서 이진탐색 방법으로 접근해야 할 듯
 
-nums = list(map(int, input().split()))
+'''
+
+n,m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+dp = []
+dp.append(0)
 
 sum = 0
-result = [0]
-for i in nums:
-    sum += i
-    result.append(sum)
-    print(sum)
+for i in range(n):
+    sum += arr[i]
+    dp.append(sum)
+
+# print(dp)
+
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    print(result[b] - result[a-1])
+    a,b = map(int, input().split())
+
+
+    # for k in range(1, n+1):
+    ans = dp[b] - dp[a-1]
+    print(ans)
+
+# print(dp)
