18250ca6065ea3a9fb2b41c779f235caa6b6fb02
ChanYoungHong <cksdud9883@naver.com>
2023-11-09 16:28:57 +0900

    누적합 복습

--- BAEKJOON/누적합 구하기/BOJ_11660_구간합구하기5.py
+++ BAEKJOON/누적합 구하기/BOJ_11660_구간합구하기5.py
@@ -2,30 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n,m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (n+1) for _ in range(n+1)]
 
+for i in range(1, n+1):
+    for j in range(1, n+1):
 
-dp = [[0]*(n+1) for i in range(n+1)]
-
-
-for i in range(1, n+1): # 1 2 3 4
-    for j in range(1, n+1): # 1 2 3 4
-        dp[i][j] = dp[i][j - 1] + dp[i-1][j] - dp[i-1][j-1] + graph[i-1][j-1]
+        dp[i][j] = board[i-1][j-1] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 
 
-for _ in range(m):
+
+for k in range(m):
     x1,y1,x2,y2 = map(int, input().split())
-
-    result = dp[x2][y2] - dp[x2][y1 -1] - dp[x1 -1 ][y2] + dp[x1-1][y1-1]
-    print(result)
-
-
-# sum = 0
-# for i in range(a-1, c-1): # 2 3
-#     for j in range(b-1, d-1): #
-#        sum += graph[i][j]
-#
-# print(sum)
-
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
