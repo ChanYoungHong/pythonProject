1313f1200af45898e24db7b030a83e501b0f111e
ChanYoungHong <cksdud9883@naver.com>
2023-11-09 19:25:04 +0900

    누적합과 투 포인터 문제결합

--- BAEKJOON/투포인터/BOJ_1806_부분합.py
+++ BAEKJOON/투포인터/BOJ_1806_부분합.py
@@ -19,22 +19,24 @@ arr = list(map(int, input().split()))
 
 left, right = 0,0
 summ = 0
-min_length = sys.maxsize
+cnt = 0
+dp = []
 
+min_length = sys.maxsize
 
 while True:
 
-    # 합이 S보다 클 경우
-
-    if summ > s:
+    # 합이 s보다 클 경우
+    if summ >= s:
+        min_length = min(min_length , right - left)
         summ -= arr[left]
-        min_length = min(min_length, right - left)
+        # summ += (arr[left] + arr[right])
         left += 1
 
     elif right == n:
         break
 
-    # 합이 S보다 작을 경우
+    # 합이 s보다 작을 경우
     else:
         summ += arr[right]
         right += 1
@@ -44,40 +46,3 @@ if min_length == sys.maxsize:
     print(0)
 else:
     print(min_length)
-
-# start = 0
-# end = len(arr) - 1
-# cnt = 0
-#
-#
-#
-# while start < end:
-#
-#     if arr[start] + arr[end] < s:
-#         end -= 1
-#     elif arr[start] + arr[end] > s:
-#         start += 1
-#         cnt += 1
-#     else:
-#         start += 1
-#         end -= 1
-#
-# print(cnt)
-
-
-# inter_num = 0
-# end = 0
-# cnt = 0
-#
-# for i in range(n):
-#
-#     while inter_num >= s and end < n:
-#         inter_num += arr[end]
-#         end += 1
-#
-#     if inter_num >= s:
-#         cnt += 1
-#
-#     inter_num -= arr[i]
-#
-# print(cnt)
