682f78d54a68875b6b173aab5984fda62bbcf10c
ChanYoungHong <cksdud9883@naver.com>
2023-11-09 16:19:26 +0900

    누적합 복습

--- BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
+++ BAEKJOON/누적합 구하기/BOJ_11659_구간합구하기4.py
@@ -3,30 +3,21 @@ import sys
 input = sys.stdin.readline
 
 '''
-n < 100000 이니깐 nlogn을 사용해서 이진탐색 방법으로 접근해야 할 듯
-
+1. 시간복잡도 - n < 100,000 아래일 때 - 투 포인터, 이진탐색, 부분합 공식 등등 사용
+2. 
 '''
 
 n,m = map(int, input().split())
 arr = list(map(int, input().split()))
 
+summ = 0
 dp = []
 dp.append(0)
-
-sum = 0
 for i in range(n):
-    sum += arr[i]
-    dp.append(sum)
+    summ += arr[i]
+    dp.append(summ)
 
 # print(dp)
-
-
 for _ in range(m):
     a,b = map(int, input().split())
-
-
-    # for k in range(1, n+1):
-    ans = dp[b] - dp[a-1]
-    print(ans)
-
-# print(dp)
+    print(dp[b] - dp[a-1])
