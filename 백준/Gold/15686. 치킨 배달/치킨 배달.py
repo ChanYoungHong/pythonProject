2ea0479e800f5d017c6b7a6f87a9b855e79cad88
chanyoung Hong <cksdud9883@naver.com>
2024-02-06 19:12:27 +0900

    [Gold V] Title: 치킨 배달, Time: 180 ms, Memory: 115432 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,32 @@
+from itertools import combinations
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+house = []
+chicken = []
+
+for i in range(n):
+    for j in range(n):
+
+        if board[i][j] == 1:
+            house.append((i, j))
+        elif board[i][j] == 2:
+            chicken.append((i, j))
+
+res = 1e9
+for i in combinations(chicken, m):
+    temp = 0
+
+    for hx, hy in house:
+        mini = 1e9
+
+        for cx, cy in i:
+            mini = min(mini, abs(hx - cx) + abs(hy - cy))
+        temp += mini
+    res = min(res, temp)
+
+print(res)
