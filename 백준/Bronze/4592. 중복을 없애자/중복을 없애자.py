50645c700293dae5d2a83ef100db9a737d4a2bf8
chanyoung Hong <cksdud9883@naver.com>
2024-02-07 13:13:08 +0900

    [Bronze II] Title: 중복을 없애자, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4592. 중복을 없애자/중복을 없애자.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+
+while True:
+
+    board = list(map(int, input().split()))
+    if board[0] == 0:
+        break
+    else:
+        n = board[0]
+
+        check_board = board[1:]
+        result = []
+
+        # for value in check_board:
+        #     if value not in result:
+        #         result.append(value)
+
+        for i in check_board:
+            if len(result) == 0:
+                result.append(i)
+            elif result[-1] != i:
+                result.append(i)
+
+
+        result.append('$')
+        print(*result)
