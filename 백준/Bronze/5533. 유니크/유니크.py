dfca1ceda13003cf22d437c4d5ac114ce49df472
chanyoung Hong <cksdud9883@naver.com>
2024-02-09 19:28:56 +0900

    [Bronze I] Title: 유니크, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5533. 유니크/유니크.py
+++ 백준/Bronze/5533. 유니크/유니크.py
@@ -1,21 +1,40 @@
-N = int(input())
-lis = [[],[],[]]
-score = [0] * N
+import sys
 
+input = sys.stdin.readline
 
-for i in range(N):
-    T = list(map(int,input().split()))
-    lis[0].append(T[0])
-    lis[1].append(T[1])
-    lis[2].append(T[2])
+'''
+1. 자신과 같은 수를 쓴 사람이 없다면, 자신이 쓴 수와 같은 점수를 얻는다.
+2.
+3.
+4.
+
+5
+100 99 98
+100 97 92
+63 89 63 -> 0 89 0 -> 89점
+99 99 99 -> 0 0 0 -> 0점
+89 97 98 ->
+'''
+
+n = int(input())
+temp = [[],[], []]
+score = [0] * n
+for _ in range(n):
+
+    t = list(map(int, input().split()))
+    temp[0].append(t[0])
+    temp[1].append(t[1])
+    temp[2].append(t[2])
 
 
 for i in range(3):
-    for j in range(N):
-        if lis[i].count(lis[i][j]) > 1:
+    for j in range(n):
+
+        if temp[i].count(temp[i][j]) > 1:
             score[j] += 0
-        else :
-            score[j] += lis[i][j]
+        else:
+            score[j] += temp[i][j]
+
 
 for i in score:
     print(i)
