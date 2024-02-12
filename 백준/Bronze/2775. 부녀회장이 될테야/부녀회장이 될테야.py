b6f328ff21140aab01a3c5c0d795d67c292cec80
chanyoung Hong <cksdud9883@naver.com>
2024-02-12 16:53:57 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -10,13 +10,15 @@ input = sys.stdin.readline
 
 t = int(input())
 
+
+
 for _ in range(t):
     k = int(input())
     n = int(input())
 
-    floor = [x for x in range(1, n+1)]
+    flr = [x for x in range(1, n+1)]
     for i in range(k):
         for j in range(1, n):
-            floor[j] += floor[j-1]
+            flr[j] += flr[j-1]
 
-    print(floor[-1])
+    print(flr[-1])
