63ee7623f0b9bff35d7d7e487314e4e65480c4f1
chanyoung Hong <cksdud9883@naver.com>
2024-02-19 19:15:40 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,6 +1,6 @@
-# import sys
-#
-# input = sys.stdin.readline
+import sys
+
+input = sys.stdin.readline
 
 '''
 1. 전공평점은 -> 학점 x 과목평점
@@ -9,71 +9,68 @@
 
 '''
 
+grade = []
 hakjum = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]
 
-# grade_points = {
-#     'A+': 4.5,
-#     'A': 4.0,
-#     'A0': 4.0,
-#     'B+': 3.5,
-#     'B': 3.0,
-#     'B0': 3.0,
-#     'C+': 2.5,
-#     'C': 2.0,
-#     'C0': 2.0,
-#     'D+': 1.5,
-#     'D': 1.0,
-#     'D0': 1.0,
-#     'F': 0.0,
-#     'P': None  # Assuming P is for Pass, so no grade point value assigned
-# }
+grade_points = {
+    'A+': 4.5,
+    'A': 4.0,
+    'A0': 4.0,
+    'B+': 3.5,
+    'B': 3.0,
+    'B0': 3.0,
+    'C+': 2.5,
+    'C': 2.0,
+    'C0': 2.0,
+    'D+': 1.5,
+    'D': 1.0,
+    'D0': 1.0,
+    'F': 0.0,
+    'P': None  # Assuming P is for Pass, so no grade point value assigned
+}
 
-total = 0
+total = 0.0
 t_credit = 0.0
-for _ in range(20):
+for i in range(20):
 
     sub, credit, grade = input().rstrip().split()
     # print(sub, credit, grade)
 
-    if grade == "A+":
+    if grade == 'A+':
         total += (float(credit) * hakjum[0])
         t_credit += float(credit)
-    elif grade == "A0":
+    elif grade == 'A0':
         total += (float(credit) * hakjum[1])
         t_credit += float(credit)
-    elif grade == "B+":
+    elif grade == 'B+':
         total += (float(credit) * hakjum[2])
         t_credit += float(credit)
-    elif grade == "B0":
+    elif grade == 'B0':
         total += (float(credit) * hakjum[3])
         t_credit += float(credit)
-    elif grade == "C+":
+    elif grade == 'C+':
         total += (float(credit) * hakjum[4])
         t_credit += float(credit)
-    elif grade == "C0":
+    elif grade == 'C0':
         total += (float(credit) * hakjum[5])
         t_credit += float(credit)
-    elif grade == "D+":
+    elif grade == 'D+':
         total += (float(credit) * hakjum[6])
         t_credit += float(credit)
-    elif grade == "D0":
+    elif grade == 'D0':
         total += (float(credit) * hakjum[7])
         t_credit += float(credit)
-    elif grade == "F":
+    elif grade == 'F':
+        total += (float(credit) * hakjum[8])
         t_credit += float(credit)
-    elif grade == "P":
+    elif grade == 'P':
         continue
 
-
-# total = (total//20)
+    # total = (total//20)
 # print(total // 20)
 # print('t_credit : ', t_credit)
 # print('total : ', total)
+total = (total / t_credit)
+total = round(total, 6)
 
-if total == 0.0:
-    # total = (total / t_credit)
-    print(total)
-
-else:
-    total = (total / t_credit)
-    print(total)
+print(total)
