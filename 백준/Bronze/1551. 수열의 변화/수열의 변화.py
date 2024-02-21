6bc868a9df274d600b0ca23b5d4cbfaed4ca0728
chanyoung Hong <cksdud9883@naver.com>
2024-02-21 13:12:26 +0900

    [Bronze I] Title: 수열의 변화, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1551. 수열의 변화/수열의 변화.py
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.py
@@ -2,11 +2,12 @@ import sys
 
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-num_list = list(map(int, input().split(',')))
+
+n,k = map(int, input().split())
+list = list(map(int, input().split(',')))
 
 for _ in range(k):
-    for i in range(n - 1):
-        num_list[i] = num_list[i + 1] - num_list[i]
-
-print(','.join(map(str, num_list[:n - k])))
+    for i in range(n -1):
+        list[i] = list[i+1] - list[i]
+        
+print(','.join(map(str, list[:n-k])))
