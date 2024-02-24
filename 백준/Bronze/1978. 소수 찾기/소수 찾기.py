3f5143919999acb22f72382e973a6ad9ac8dd3cd
chanyoung Hong <cksdud9883@naver.com>
2024-02-24 13:01:27 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -5,20 +5,30 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 
+# cnt = 0
+# for i in nums:
+#     if i == 2 or i == 3:
+#         cnt += 1
+#     elif (i%2) == 1 and (i%3) == 1:
+#         cnt += 1
+#     elif (i%2) == 1 and (i%3) != 0:
+#         cnt += 1
+#     elif (i%2) != 0 and (i%3) == 1:
+#         cnt += 1
+#
+# print(cnt)
+
 cnt = 0
 
-for i in nums:
+for num in nums:
     error = 0
-    
-    if i > 1:
-        
-        for j in range(2, i):
-            
-            if i%j == 0:
+
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
                 error += 1
-        
+
         if error == 0:
             cnt += 1
 
 print(cnt)
-
