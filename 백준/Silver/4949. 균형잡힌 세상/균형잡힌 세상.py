417f7a09304d5ba4ead3490e94b31974f1093023
chanyoung Hong <cksdud9883@naver.com>
2024-03-05 19:07:41 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,38 @@
+
+import sys
+
+input = sys.stdin.readline
+
+
+
+while True:
+
+    word = input().rstrip()
+    temp = []
+    if word == '.':
+        break
+
+    for i in word:
+
+        if i == '(' or i == '[':
+            temp.append(i)
+
+        elif i == ')':
+            if len(temp) != 0 and temp[-1] == '(':
+                temp.pop()
+            else:
+                temp.append(i)
+                break
+
+        elif i == ']':
+            if len(temp) != 0 and temp[-1] == '[':
+                temp.pop()
+            else:
+                temp.append(i)
+                break
+
+
+    if len(temp) == 0:
+        print('yes')
+    else:
+        print('no')
