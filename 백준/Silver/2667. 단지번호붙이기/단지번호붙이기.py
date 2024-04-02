e6459ed35f01498b2d5775cddccdbe309ff1c53f
chanyoung Hong <cksdud9883@naver.com>
2024-04-02 11:22:28 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,41 +2,43 @@ import sys
 
 input = sys.stdin.readline
 
+
+
 n = int(input())
 board = [list(map(int, input().rstrip())) for _ in range(n)]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
+def dfs(x,y):
 
-def dfs(x, y):
     global cnt
-    cnt += 1
 
     for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+        nx = dx[i] + x
+        ny = dy[i] + y
 
-        if 0 <= nx < n and 0 <= ny < n:
-            if board[nx][ny] == 1:
-                board[nx][ny] = 0
-                dfs(nx, ny)
+        if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 1:
+            board[nx][ny] = 0
+            dfs(nx,ny)
+            cnt += 1
 
 
     return cnt
 
-cnt = 0
-res = []
 
+
+cnt = 0
+ans = []
 for i in range(n):
     for j in range(n):
 
         if board[i][j] == 1:
             board[i][j] = 0
-            res.append(dfs(i, j))
+            ans.append(dfs(i,j))
             cnt = 0
 
-print(len(res))
-res.sort()
-for z in res:
-    print(z)
+print(len(ans))
+ans.sort()
+for i in ans:
+    print(i+1)
