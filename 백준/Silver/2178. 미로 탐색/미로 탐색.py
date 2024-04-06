a313d4e951907146917637993b7f99fab6ecb1fa
chanyoung Hong <cksdud9883@naver.com>
2024-04-06 15:36:48 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+board = [list(map(int, input().rstrip())) for _ in range(n)]
+
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def bfs(x,y):
+
+    q = deque()
+    q.append((x,y))
+
+    while q:
+
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if board[nx][ny] == 1:
+                    board[nx][ny] = board[x][y] + 1
+                    q.append((nx,ny))
+
+    return board[-1][-1]
+
+
+print(bfs(0,0))
+
